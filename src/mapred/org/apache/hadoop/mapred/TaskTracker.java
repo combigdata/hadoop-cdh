3ce342baafd3774e4d920a7fcb49a7e091a0cad1
Todd Lipcon <todd@cloudera.com>
2009-09-28 13:36:31 -0700

    MAPREDUCE-964. Fixed start and finish times of TaskStatus to be consistent, thereby fixing inconsistencies in metering tasks.
    Patch: http://issues.apache.org/jira/secure/attachment/12420539/mapreduce-964-ydist.patch
    Patch: http://issues.apache.org/jira/secure/attachment/12420893/mapreduce-964-ydist-1.patch
    Author: Sreekanth Ramakrishnan
    Ref: YDH

--- src/mapred/org/apache/hadoop/mapred/TaskTracker.java
+++ src/mapred/org/apache/hadoop/mapred/TaskTracker.java
@@ -2433,6 +2433,7 @@ public class TaskTracker
           taskStatus.setRunState(TaskStatus.State.KILLED);
         }
       }
+      taskStatus.setFinishTime(System.currentTimeMillis());
       removeFromMemoryManager(task.getTaskID());
       releaseSlot();
     }
