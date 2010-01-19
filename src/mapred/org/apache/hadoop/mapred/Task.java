44df01f8009c02e7346b69389ee8a26ef824bba2
Todd Lipcon <todd@cloudera.com>
2010-01-19 12:11:07 +0530

    MAPREDUCE-842. Per-job local data on the TaskTracker node should have right access-control
    Patch: https://issues.apache.org/jira/secure/attachment/12430697/842.20S-4.patch
    Author: Vinod K V
    Ref: CDH-648

--- src/mapred/org/apache/hadoop/mapred/Task.java
+++ src/mapred/org/apache/hadoop/mapred/Task.java
@@ -177,7 +177,6 @@ abstract public class Task implements Writable, Configurable {
                                                     TaskStatus.Phase.MAP : 
                                                     TaskStatus.Phase.SHUFFLE, 
                                                   counters);
-    this.mapOutputFile.setJobId(taskId.getJobID());
     spilledRecordsCounter = counters.findCounter(Counter.SPILLED_RECORDS);
   }
 
@@ -403,7 +402,6 @@ abstract public class Task implements Writable, Configurable {
     partition = in.readInt();
     numSlotsRequired = in.readInt();
     taskStatus.readFields(in);
-    this.mapOutputFile.setJobId(taskId.getJobID()); 
     skipRanges.readFields(in);
     currentRecIndexIterator = skipRanges.skipRangeIterator();
     currentRecStartIndex = currentRecIndexIterator.next();
