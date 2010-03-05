69059680d9e0df4004ac7199c2c54ea658c35173
Todd Lipcon <todd@cloudera.com>
2010-03-05 05:58:25 +0000

    HDFS-814. Add an api to get the visible length of a DFSDataInputStream.
    Patch: http://issues.apache.org/jira/secure/attachment/12437934/getLength-yahoo-0.20.patch
    Patch: http://issues.apache.org/jira/secure/attachment/12438026/privateInputStream.patch
    Author: Tsz Wo (Nicholas), SZE
    Ref: YDH

--- src/hdfs/org/apache/hadoop/hdfs/DFSClient.java
+++ src/hdfs/org/apache/hadoop/hdfs/DFSClient.java
@@ -2193,8 +2193,11 @@ public class DFSClient implements FSConstants, java.io.Closeable {
       throw new IOException("Mark/reset not supported");
     }
   }
-    
-  static class DFSDataInputStream extends FSDataInputStream {
+
+  /**
+   * The Hdfs implementation of {@link FSDataInputStream}
+   */
+  public static class DFSDataInputStream extends FSDataInputStream {
     DFSDataInputStream(DFSInputStream in)
       throws IOException {
       super(in);
@@ -2221,6 +2224,12 @@ public class DFSClient implements FSConstants, java.io.Closeable {
       return ((DFSInputStream)in).getAllBlocks();
     }
 
+    /**
+     * @return The visible length of the file.
+     */
+    public long getVisibleLength() throws IOException {
+      return ((DFSInputStream)in).getFileLength();
+    }
   }
 
   /****************************************************************
