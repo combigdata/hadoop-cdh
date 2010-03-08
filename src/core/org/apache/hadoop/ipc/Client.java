eb5a68ab5a10bd85527cd4495a17687a826af698
Todd Lipcon <todd@cloudera.com>
2010-03-07 23:14:19 -0800

    HADOOP-6612. Protocols RefreshUserToGroupMappingsProtocol and RefreshAuthorizationPolicyProtocol will fail with security enabled
    Patch: https://issues.apache.org/jira/secure/attachment/12437809/HADOOP-6612-BP20.patch
    Author: Boris Shkolnik
    Ref: CDH-648

--- src/core/org/apache/hadoop/ipc/Client.java
+++ src/core/org/apache/hadoop/ipc/Client.java
@@ -262,6 +262,11 @@ public class Client {
         if (krbInfo != null) {
           String serverKey = krbInfo.serverPrincipal();
           if (serverKey != null) {
+            if(LOG.isDebugEnabled()) {
+            LOG.debug("server principal key for protocol="
+                + protocol.getCanonicalName() + " is " + serverKey + 
+                " and val =" + conf.get(serverKey));
+            }
             serverPrincipal = conf.get(serverKey);
           }
         }
