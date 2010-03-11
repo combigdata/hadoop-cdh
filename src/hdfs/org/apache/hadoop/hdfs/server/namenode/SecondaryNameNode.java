6d83d8407d13f0e4b8b26f74bfbbd592b4c906ee
Todd Lipcon <todd@cloudera.com>
2010-03-11 14:38:56 -0800

    HDFS-1033. In secure clusters, NN and SNN should verify that the remote principal during image and edits transfer
    Patch: https://issues.apache.org/jira/secure/attachment/12438477/HDFS-1033-Y20.patch
    Author: Jakob Homan
    Ref: CDH-648

--- src/hdfs/org/apache/hadoop/hdfs/server/namenode/SecondaryNameNode.java
+++ src/hdfs/org/apache/hadoop/hdfs/server/namenode/SecondaryNameNode.java
@@ -121,8 +121,8 @@ public class SecondaryNameNode implements Runnable {
    */
   public SecondaryNameNode(Configuration conf)  throws IOException {
     DFSUtil.login(conf, 
-        DFSConfigKeys.DFS_NAMENODE_KEYTAB_FILE_KEY,
-        DFSConfigKeys.DFS_NAMENODE_USER_NAME_KEY);
+        DFSConfigKeys.DFS_SECONDARY_NAMENODE_KEYTAB_FILE_KEY,
+        DFSConfigKeys.DFS_SECONDARY_NAMENODE_USER_NAME_KEY);
     try {
       initialize(conf);
     } catch(IOException e) {
@@ -162,8 +162,8 @@ public class SecondaryNameNode implements Runnable {
 
     // initialize the webserver for uploading files.
     // Kerberized SSL servers must be run from the host principal...
-    DFSUtil.login(conf, DFSConfigKeys.DFS_NAMENODE_KEYTAB_FILE_KEY, 
-        DFSConfigKeys.DFS_NAMENODE_KRB_HTTPS_USER_NAME_KEY);
+    DFSUtil.login(conf, DFSConfigKeys.DFS_SECONDARY_NAMENODE_KEYTAB_FILE_KEY, 
+        DFSConfigKeys.DFS_SECONDARY_NAMENODE_KRB_HTTPS_USER_NAME_KEY);
     UserGroupInformation ugi = UserGroupInformation.getLoginUser();
     try {
       infoServer = ugi.doAs(new PrivilegedExceptionAction<HttpServer>() {
