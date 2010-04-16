c2b68855b127c7dc532ce836fa60dc5c1836f6ec
Todd Lipcon <todd@cloudera.com>
2010-04-16 14:10:31 -0700

    MAPREDUCE-1317. Reducing memory consumption of rumen objects. Contributed by Hong Tang.
    Patch: https://issues.apache.org/jira/secure/attachment/12442004/mapreduce-1317-yhadoo-20.1xx.patch.
    Patch: https://issues.apache.org/jira/secure/attachment/12443927/3623945-yahoo-20-1xx.patch
    
    Author: Hong Tang
    Ref: YDH

--- src/tools/org/apache/hadoop/tools/rumen/LoggedTaskAttempt.java
+++ src/tools/org/apache/hadoop/tools/rumen/LoggedTaskAttempt.java
@@ -135,7 +135,7 @@ public class LoggedTaskAttempt implements DeepCompare {
   }
 
   void setHostName(String hostName) {
-    this.hostName = hostName;
+    this.hostName = (hostName == null) ? null : hostName.intern();
   }
 
   public long getHdfsBytesRead() {
