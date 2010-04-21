092bcd174dbf609f5002078490c357462e0ce8b1
Konstantin Shvachko <shv@apache.org>
2010-04-21 03:05:45 +0000

    HDFS-909. Fix race in edit log rolling
    Description: Fixes a race condition when rolling edit logs that can corrupt
                 the logs.
    Reason: Potential namenode metadata corruption bug.
    Author: Todd Lipcon
    Ref: CDH-1174

--- src/hdfs/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ src/hdfs/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -4411,6 +4411,7 @@ public class FSNamesystem implements FSConstants, FSNamesystemMBean {
    * @throws IOException
    */
   synchronized void enterSafeMode() throws IOException {
+    getEditLog().logSyncAll();
     if (!isInSafeMode()) {
       safeMode = new SafeModeInfo();
       return;
