0ec1d6ed85a30327c657c2418932728d0e4e98df
Todd Lipcon <todd@lipcon.org>
2010-05-12 21:33:45 -0700

    HADOOP-6254. Slow reads cause s3n to fail with SocketTimeoutException
    Reason: Bug fix for users of s3n:// file system
    Author: Andrew Hitchcock
    Ref: CDH-1035

--- src/test/org/apache/hadoop/fs/s3native/NativeS3FileSystemContractBaseTest.java
+++ src/test/org/apache/hadoop/fs/s3native/NativeS3FileSystemContractBaseTest.java
@@ -19,12 +19,14 @@
 package org.apache.hadoop.fs.s3native;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystemContractBaseTest;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.s3native.NativeS3FileSystem.NativeS3FsInputStream;
 
 public abstract class NativeS3FileSystemContractBaseTest
   extends FileSystemContractBaseTest {
