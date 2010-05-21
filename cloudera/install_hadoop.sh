95d64157f05d467dad3e1190a5cba2a3f89b0925
Eli Collins <eli@cloudera.com>
2010-05-20 17:15:13 -0700

    CLOUDERA-BUILD. Rename the fuse_dfs wrapper.
    Description: Rename the fuse_dfs wrapper to hadoop-fuse-dfs.
    
    Reason: Improvement
    Author: Alex Newman
    Ref: CDH-1103

--- cloudera/install_hadoop.sh
+++ cloudera/install_hadoop.sh
@@ -191,13 +191,13 @@ cp ${BUILD_DIR}/../../docs/sqoop/sqoop.1.gz $MAN_DIR/man1/sqoop-$APACHE_BRANCH.1
 ############################################################
 
 if [ ! -z "$NATIVE_BUILD_STRING" ]; then
-  cp ${CLOUDERA_SOURCE_DIR}/fuse_dfs.1.gz $MAN_DIR/man1/
+  cp ${CLOUDERA_SOURCE_DIR}/hadoop-fuse-dfs.1.gz $MAN_DIR/man1/
   # Fuse 
   mkdir -p $LIB_DIR/bin
   mv  ${BUILD_DIR}/contrib/fuse-dfs/* $LIB_DIR/bin
   rmdir ${BUILD_DIR}/contrib/fuse-dfs 
 
-  fuse_wrapper=${BIN_DIR}/fuse_dfs
+  fuse_wrapper=${BIN_DIR}/hadoop-fuse-dfs
   cat > $fuse_wrapper << EOF
 #!/bin/bash
 
