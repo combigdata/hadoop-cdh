11d842c61eb63c156e1c3f753d795868bbd2fa0a
Todd Lipcon <todd@cloudera.com>
2010-09-23 13:03:53 -0700

    HADOOP-6815. refreshSuperUserGroupsConfiguration should use server side configuration for the refresh
    Author: Boris Shkolnik
    Ref: CDH-648

--- src/hdfs/org/apache/hadoop/hdfs/tools/DFSAdmin.java
+++ src/hdfs/org/apache/hadoop/hdfs/tools/DFSAdmin.java
@@ -680,7 +680,7 @@ public class DFSAdmin extends FsShell {
                                              RefreshUserMappingsProtocol.class));
     
     // Refresh the user-to-groups mappings
-    refreshProtocol.refreshUserToGroupsMappings(conf);
+    refreshProtocol.refreshUserToGroupsMappings();
     
     return 0;
   }
@@ -710,7 +710,7 @@ public class DFSAdmin extends FsShell {
                        RefreshUserMappingsProtocol.class));
     
     // Refresh the user-to-groups mappings
-    refreshProtocol.refreshSuperUserGroupsConfiguration(conf);
+    refreshProtocol.refreshSuperUserGroupsConfiguration();
     
     return 0;
   }
