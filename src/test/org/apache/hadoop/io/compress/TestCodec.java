bd3ea0cbe457e49b5e1bbdf4d7dc57599feb3537
Alejandro Abdelnur <tucu00@gmail.com>
2011-06-24 11:33:16 -0700

    HADOOP-7206 SNAPPY backport
    Adds Snappy compression support.
    
        Reason: New Functionality
        Author: Issei Yoshida, Alejandro Abdelnur
        Ref: CDH-3039
    
    HADOOP-7206 SNAPPY backport

--- src/test/org/apache/hadoop/io/compress/TestCodec.java
+++ src/test/org/apache/hadoop/io/compress/TestCodec.java
@@ -50,6 +50,7 @@ import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.SequenceFile.CompressionType;
+import org.apache.hadoop.io.compress.snappy.LoadSnappy;
 import org.apache.hadoop.io.compress.CompressionOutputStream;
 import org.apache.hadoop.io.compress.CompressorStream;
 import org.apache.hadoop.io.compress.zlib.BuiltInZlibDeflater;
@@ -64,6 +65,7 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -92,6 +94,19 @@ public class TestCodec {
   public void testBZip2Codec() throws IOException {
     codecTest(conf, seed, 0, "org.apache.hadoop.io.compress.BZip2Codec");
     codecTest(conf, seed, count, "org.apache.hadoop.io.compress.BZip2Codec");
+  }
+  
+  @Test
+  public void testSnappyCodec() throws IOException {
+    if (LoadSnappy.isAvailable()) {
+      if (LoadSnappy.isLoaded()) {
+        codecTest(conf, seed, 0, "org.apache.hadoop.io.compress.SnappyCodec");
+        codecTest(conf, seed, count, "org.apache.hadoop.io.compress.SnappyCodec");
+      }
+      else {
+        Assert.fail("Snappy native available but Hadoop native not");
+      }
+    }
   }
 
   @Test
