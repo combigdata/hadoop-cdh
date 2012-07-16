5a604c329ffd7965f726de2d344c5e55a7323a85
Todd Lipcon <todd@apache.org>
2012-07-16 18:54:46 +0000

    HDFS-3625. Fix TestBackupNode by properly initializing edit log during startup. Contributed by Junping Du.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1362189 13f79535-47bb-0310-9956-ffa450edef68
    (cherry picked from commit bdebfc2f21d14cfcf48c31ea8950ca9279c3b76b)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/BackupNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/BackupNode.java
@@ -346,6 +346,7 @@ public class BackupNode extends NameNode {
     } else {
       nsInfo.validateStorage(storage);
     }
+    bnImage.initEditLog();
     setRegistration();
     NamenodeRegistration nnReg = null;
     while(!isStopRequested()) {
