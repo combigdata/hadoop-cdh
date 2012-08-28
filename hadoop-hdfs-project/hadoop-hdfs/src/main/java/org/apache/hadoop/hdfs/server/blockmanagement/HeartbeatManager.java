9ab6ec8551c219f2e1351edfd071362b5ae9ced7
Aaron Twining Myers <atm@apache.org>
2012-08-28 17:02:33 +0000

    HDFS-3860. HeartbeatManager#Monitor may wrongly hold the writelock of namesystem. Contributed by Jing Zhao.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1378229 13f79535-47bb-0310-9956-ffa450edef68
    (cherry picked from commit 73fc2655bb241c85ea33fff8b8bebe566cfcc88d)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/HeartbeatManager.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/HeartbeatManager.java
@@ -224,10 +224,10 @@ class HeartbeatManager implements DatanodeStatistics {
       if (!allAlive) {
         // acquire the fsnamesystem lock, and then remove the dead node.
         namesystem.writeLock();
-        if (namesystem.isInSafeMode()) {
-          return;
-        }
         try {
+          if (namesystem.isInSafeMode()) {
+            return;
+          }
           synchronized(this) {
             dm.removeDeadDatanode(dead);
           }
