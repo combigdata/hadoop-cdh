d2eefe3a3d5d681bfdecd0bd909c13536b1b0af7
Eli Collins <eli@apache.org>
2012-09-12 19:15:38 +0000

    HDFS-3928. MiniDFSCluster should reset the first ExitException on shutdown. Contributed by Eli Collins
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1384092 13f79535-47bb-0310-9956-ffa450edef68
    (cherry picked from commit ff375e49465ec9c0fe67c6c0d7c8b2435f41c2a7)

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/MiniDFSCluster.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/MiniDFSCluster.java
@@ -1354,6 +1354,7 @@ public class MiniDFSCluster {
       if (ExitUtil.terminateCalled()) {
         LOG.fatal("Test resulted in an unexpected exit",
             ExitUtil.getFirstExitException());
+        ExitUtil.resetFirstExitException();
         throw new AssertionError("Test resulted in an unexpected exit");
       }
     }
