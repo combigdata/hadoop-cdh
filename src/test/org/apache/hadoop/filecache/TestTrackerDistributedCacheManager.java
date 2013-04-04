apache/hadoop-common

MAPREDUCE-2409. Distinguish distributed cache artifacts localized as files, archives. Contributed by Siddharth Seth
git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.20-security@1087209 13f79535-47bb-0310-9956-ffa450edef68

src/test/org/apache/hadoop/filecache/TestTrackerDistributedCacheManager.java
@@ -319,7 +319,51 @@ public class TestTrackerDistributedCacheManager extends TestCase {
     checkLocalizedPath(true);
     checkLocalizedPath(false);
   }
-  
+
+  public void testSameNameFileArchiveCache() throws IOException,
+      InterruptedException {
+    if (!canRun()) {
+      return;
+    }
+    TrackerDistributedCacheManager manager = new TrackerDistributedCacheManager(
+        conf, taskController);
+    String userName = getJobOwnerName();
+    File workDir = new File(TEST_ROOT_DIR, "workdir");
+    Path cacheFile = new Path(TEST_ROOT_DIR, "fileArchiveCacheFile");
+
+    createPublicTempFile(cacheFile);
+    Configuration conf1 = new Configuration(conf);
+    conf1.set("user.name", userName);
+
+    DistributedCache.addCacheFile(cacheFile.toUri(), conf1);
+    DistributedCache.addCacheArchive(cacheFile.toUri(), conf1);
+    TrackerDistributedCacheManager.determineTimestamps(conf1);
+    TrackerDistributedCacheManager.determineCacheVisibilities(conf1);
+    dumpState(conf1);
+
+    TaskDistributedCacheManager handle = manager
+        .newTaskDistributedCacheManager(new JobID("jt", 1), conf1);
+    handle.setupCache(conf1, TaskTracker.getPublicDistributedCacheDir(),
+        TaskTracker.getPrivateDistributedCacheDir(userName));
+
+    TaskDistributedCacheManager.CacheFile cFile = handle.getCacheFiles().get(0);
+    TaskDistributedCacheManager.CacheFile cArchive = handle.getCacheFiles()
+        .get(1);
+
+    String distCacheDir = TaskTracker.getPublicDistributedCacheDir();
+
+    Path localizedPathForFile = manager.getLocalCache(cacheFile.toUri(), conf1,
+        distCacheDir, fs.getFileStatus(cacheFile), false, cFile.timestamp,
+        true, cFile);
+
+    Path localizedPathForArchive = manager.getLocalCache(cacheFile.toUri(),
+        conf1, distCacheDir, fs.getFileStatus(cacheFile), true,
+        cArchive.timestamp, true, cArchive);
+    assertNotSame("File and Archive resolve to the same path: "
+        + localizedPathForFile + ". Should differ.", localizedPathForFile,
+        localizedPathForArchive);
+  }
+
   private void appendStringArray(StringBuilder buffer, String[] data) {
     if (data != null && data.length != 0) {
       buffer.append(data[0]);

combigdata/hadoop-cdh

(cherry picked from commit 093cf31c10a2d05fd82ec9406ccb8e4b20a582e0)
(cherry picked from commit c2a4f8836d88554f4e91a3e4901a40d743eae8b6)
Conflicts:
	src/test/org/apache/hadoop/filecache/TestTrackerDistributedCacheManager.java
(cherry picked from commit f056597ebd04d267098f406990dd051b07ebb9ca)

src/test/org/apache/hadoop/filecache/TestTrackerDistributedCacheManager.java
@@ -338,6 +338,49 @@ public class TestTrackerDistributedCacheManager extends TestCase {
     checkLocalizedPath(false);
   }
 
+  public void testSameNameFileArchiveCache() throws IOException,
+      InterruptedException {
+    if (!canRun()) {
+      return;
+    }
+    TrackerDistributedCacheManager manager = new TrackerDistributedCacheManager(
+        conf, taskController);
+    String userName = getJobOwnerName();
+    File workDir = new File(TEST_ROOT_DIR, "workdir");
+    Path cacheFile = new Path(TEST_ROOT_DIR, "fileArchiveCacheFile");
+
+    createPublicTempFile(cacheFile);
+    Configuration conf1 = new Configuration(conf);
+    conf1.set("user.name", userName);
+
+    DistributedCache.addCacheFile(cacheFile.toUri(), conf1);
+    DistributedCache.addCacheArchive(cacheFile.toUri(), conf1);
+    TrackerDistributedCacheManager.determineTimestampsAndCacheVisibilities(conf1);
+    dumpState(conf1);
+
+    TaskDistributedCacheManager handle = manager
+        .newTaskDistributedCacheManager(new JobID("jt", 1), conf1);
+    handle.setupCache(conf1, TaskTracker.getPublicDistributedCacheDir(),
+        TaskTracker.getPrivateDistributedCacheDir(userName));
+
+    TaskDistributedCacheManager.CacheFile cFile = handle.getCacheFiles().get(0);
+    TaskDistributedCacheManager.CacheFile cArchive = handle.getCacheFiles()
+        .get(1);
+
+    String distCacheDir = TaskTracker.getPublicDistributedCacheDir();
+
+    Path localizedPathForFile = manager.getLocalCache(cacheFile.toUri(), conf1,
+        distCacheDir, fs.getFileStatus(cacheFile), false, cFile.timestamp,
+        true, cFile);
+
+    Path localizedPathForArchive = manager.getLocalCache(cacheFile.toUri(),
+        conf1, distCacheDir, fs.getFileStatus(cacheFile), true,
+        cArchive.timestamp, true, cArchive);
+    assertNotSame("File and Archive resolve to the same path: "
+        + localizedPathForFile + ". Should differ.", localizedPathForFile,
+        localizedPathForArchive);
+  }
+
   private void appendStringArray(StringBuilder buffer, String[] data) {
     if (data != null && data.length != 0) {
       buffer.append(data[0]);