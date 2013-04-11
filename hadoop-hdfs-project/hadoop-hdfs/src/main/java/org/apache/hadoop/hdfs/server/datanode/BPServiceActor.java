a3359d43a77821ecf845e019d3f3139f3c2910be
David S. Wang <dsw@cloudera.com>
2013-04-11 10:36:06 -0700

    HDFS-4656. DN heartbeat loop can be briefly tight. Contributed by Aaron T. Myers.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1463358 13f79535-47bb-0310-9956-ffa450edef68
    (cherry picked from commit 8742d3b4b5dd5ebfcfd7afa97d9cb8e56000719e)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/BPServiceActor.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/BPServiceActor.java
@@ -508,7 +508,7 @@ class BPServiceActor implements Runnable {
         //
         // Every so often, send heartbeat or block-report
         //
-        if (startTime - lastHeartbeat > dnConf.heartBeatInterval) {
+        if (startTime - lastHeartbeat >= dnConf.heartBeatInterval) {
           //
           // All heartbeat messages include following info:
           // -- Datanode name
