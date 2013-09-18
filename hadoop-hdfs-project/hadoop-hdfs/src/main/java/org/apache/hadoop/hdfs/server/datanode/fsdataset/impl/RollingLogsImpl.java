abf58c39df0cff9c37c596da7050b28629877c84
Arpit Agarwal <arp@apache.org>
2013-09-18 20:21:48 +0000

    HDFS-5031. Merge change r1524553 from trunk to branch-2
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1524556 13f79535-47bb-0310-9956-ffa450edef68

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/RollingLogsImpl.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/RollingLogsImpl.java
@@ -128,6 +128,7 @@ class RollingLogsImpl implements RollingLogs {
    */
   private class Reader implements RollingLogs.LineIterator {
     private File file;
+    private File lastReadFile;
     private BufferedReader reader;
     private String line;
     private boolean closed = false;
@@ -141,6 +142,11 @@ class RollingLogsImpl implements RollingLogs {
     @Override
     public boolean isPrevious() {
       return file == prev;
+    }
+
+    @Override
+    public boolean isLastReadFromPrevious() {
+      return lastReadFile == prev;
     }
 
     private boolean openFile() throws IOException {
@@ -196,6 +202,7 @@ class RollingLogsImpl implements RollingLogs {
     public String next() {
       String curLine = line;
       try {
+        lastReadFile = file;
         readNext();
       } catch (IOException e) {
         DataBlockScanner.LOG.warn("Failed to read next line.", e);
