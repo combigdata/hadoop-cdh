3542361eee4ecb71eae50235b807cb9658a11491
Karthik Kambatla <kasha@apache.org>
2014-05-22 06:13:24 +0000

    YARN-2089. FairScheduler: QueuePlacementPolicy and QueuePlacementRule are missing audience annotations. (Zhihai Xu via kasha)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/trunk@1596765 13f79535-47bb-0310-9956-ffa450edef68
    (cherry picked from commit 71fd72e5564187edcf86b0e190702fb61ad892cf)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/QueuePlacementRule.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/QueuePlacementRule.java
@@ -23,12 +23,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.hadoop.classification.InterfaceAudience.Private;
+import org.apache.hadoop.classification.InterfaceStability.Unstable;
 import org.apache.hadoop.security.Groups;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
+@Private
+@Unstable
 public abstract class QueuePlacementRule {
   protected boolean create;
   
