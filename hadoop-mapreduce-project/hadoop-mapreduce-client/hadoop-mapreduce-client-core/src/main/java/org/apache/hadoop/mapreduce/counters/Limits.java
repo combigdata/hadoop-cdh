e8895df1932568faeb10186eca34f1f1505c277f
Robert Kanter <rkanter@cloudera.com>
2014-06-02 13:55:39 -0700

    CLOUDERA-BUILD. Revert "CLOUDERA-BUILD. preliminary MAPREDUCE-5875. Make Counter limits consistent conf across JobClient, MRAppMaster, and YarnChild (CDH-18794)"
    This reverts commit d533d6a198926f6c1a83dea8712ed3f519e5852b.

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/counters/Limits.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/counters/Limits.java
@@ -125,9 +125,4 @@ public class Limits {
   public synchronized LimitExceededException violation() {
     return firstViolation;
   }
-
-  public static synchronized void reset(Configuration conf) {
-    isInited = false;
-    init(conf);
-  }
 }
