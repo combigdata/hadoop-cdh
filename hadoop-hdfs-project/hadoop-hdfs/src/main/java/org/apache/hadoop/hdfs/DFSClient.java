bd601cca12658d5d7fceb8a3a6c3f3328610cc96
Alejandro Abdelnur <tucu@apache.org>
2014-09-05 22:33:48 -0700

    HDFS-6986. DistributedFileSystem must get delegation tokens from configured KeyProvider. (zhz via tucu)
    (cherry picked from commit 035112f25133343a55f9c65e0577a2230954dae8)
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/CHANGES.txt

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSClient.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSClient.java
@@ -3074,4 +3074,8 @@ public class DFSClient implements java.io.Closeable, RemotePeerFactory,
   DFSHedgedReadMetrics getHedgedReadMetrics() {
     return HEDGED_READ_METRIC;
   }
+
+  public KeyProviderCryptoExtension getKeyProvider() {
+    return provider;
+  }
 }
