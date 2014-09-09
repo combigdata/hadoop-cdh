80ca2afc1062f374b252314b3d0bc092c49c561f
Yongjun Zhang <yzhang@cloudera.com>
2014-09-09 15:21:10 -0700

    CDH-21363. Relax HDFS-6776 requirement so user doesn't need to upgrade insecure cluster
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/web/WebHdfsFileSystem.java

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/web/WebHdfsFileSystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/web/WebHdfsFileSystem.java
@@ -1031,17 +1031,32 @@ public class WebHdfsFileSystem extends FileSystem
   @Override
   public Token<DelegationTokenIdentifier> getDelegationToken(
       final String renewer) throws IOException {
-    final HttpOpParam.Op op = GetOpParam.Op.GETDELEGATIONTOKEN;
-    final Map<?, ?> m = run(op, null, new RenewerParam(renewer));
-    final Token<DelegationTokenIdentifier> token = JsonUtil.toDelegationToken(m);
-    if (token != null) {
-      token.setService(tokenServiceName);
-    } else {
-      if (disallowFallbackToInsecureCluster) {
-        throw new AccessControlException(CANT_FALLBACK_TO_INSECURE_MSG);
+    try {
+      final HttpOpParam.Op op = GetOpParam.Op.GETDELEGATIONTOKEN;
+      final Map<?, ?> m = run(op, null, new RenewerParam(renewer));
+      final Token<DelegationTokenIdentifier> token = JsonUtil.toDelegationToken(m);
+      if (token != null) {
+        token.setService(tokenServiceName);
+      } else {
+        if (disallowFallbackToInsecureCluster) {
+          throw new AccessControlException(CANT_FALLBACK_TO_INSECURE_MSG);
+        }
       }
+      return token;
+    } catch (IOException ioe) {
+      if (ioe.getMessage().startsWith("Failed to get the token for")) {
+        // cluster that is insecure and doesn't have the fix of HDFS-6776
+        // throws IOException with msg that starts with
+        // ""Failed to get the token for" when requested for delegation
+        // token. Catch it here and return null delegation token if 
+        // fallback is allowed
+        if (disallowFallbackToInsecureCluster) {
+          throw new AccessControlException(CANT_FALLBACK_TO_INSECURE_MSG);
+        }
+        return null;
+      }
+      throw ioe;
     }
-    return token;
   }
 
   @Override
