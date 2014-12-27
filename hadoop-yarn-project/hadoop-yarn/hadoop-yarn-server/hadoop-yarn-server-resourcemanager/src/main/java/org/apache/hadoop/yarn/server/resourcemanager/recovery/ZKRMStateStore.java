a93ecb6aa819ca50329ebef725a6c31ce62cc28c
Jian He <jianhe@apache.org>
2014-12-26 17:59:58 -0800

    YARN-2992. ZKRMStateStore crashes due to session expiry. Contributed by Karthik Kambatla (cherry picked from commit 1454efe5d4fe4214ec5ef9142d55dbeca7dab953)
    (cherry picked from commit ca0349b87ab1b2d0d2b9dc93de7806d26713165c)
    (cherry picked from commit 5c9d8b370050a4dd7bd9d45098924382930d5d39)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/recovery/ZKRMStateStore.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/recovery/ZKRMStateStore.java
@@ -1074,6 +1074,8 @@ public class ZKRMStateStore extends RMStateStore {
       switch (code) {
         case CONNECTIONLOSS:
         case OPERATIONTIMEOUT:
+        case SESSIONEXPIRED:
+        case SESSIONMOVED:
           return true;
         default:
           break;
@@ -1098,6 +1100,7 @@ public class ZKRMStateStore extends RMStateStore {
           if (shouldRetry(ke.code()) && ++retry < numRetries) {
             LOG.info("Retrying operation on ZK. Retry no. " + retry);
             Thread.sleep(zkRetryInterval);
+            createConnection();
             continue;
           }
           LOG.info("Maxed out ZK retries. Giving up!");
