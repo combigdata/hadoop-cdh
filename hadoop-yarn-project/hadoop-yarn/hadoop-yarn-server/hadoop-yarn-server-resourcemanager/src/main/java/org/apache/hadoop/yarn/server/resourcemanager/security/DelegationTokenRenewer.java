9a6470cd4a434500ef886d459dafcb2adb03924a
Jian He <jianhe@apache.org>
2015-04-13 14:07:17 -0700

    YARN-3472. Fixed possible leak in DelegationTokenRenewer#allTokens. Contributed by Rohith Sharmaks
    (cherry picked from commit a1afbc48b53f6bdbd30dc8eb56a7621d49c5d6db)
    (cherry picked from commit 5cf35b98e1e751b28e5b2ddeb75b9be13cf98734)
    
    Change-Id: I3e37c2574c47cfddbff2a5a1f912ab2573080c51

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/security/DelegationTokenRenewer.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/security/DelegationTokenRenewer.java
@@ -615,6 +615,7 @@ public class DelegationTokenRenewer extends AbstractService {
             DelegationTokenToRenew t = iter.next();
             if (t.token.getKind().equals(HDFS_DELEGATION_KIND)) {
               iter.remove();
+              allTokens.remove(t.token);
               t.cancelTimer();
               LOG.info("Removed expiring token " + t);
             }
