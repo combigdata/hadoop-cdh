1affb71507f3d4bb7c2e504dc727040d54d8417a
Arpit Agarwal <arp@apache.org>
2015-04-24 16:47:48 -0700

    HDFS-8211. DataNode UUID is always null in the JMX counter. (Contributed by Anu Engineer)
    (cherry picked from commit 932cff610a5d65618c6c3e1a8bf15a0d11cb7d33)
    
    Change-Id: I0a5c0fd299a7b436c4922fc0e56e4b713759b9ba

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DataNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DataNode.java
@@ -1219,7 +1219,7 @@ public class DataNode extends ReconfigurableBase
    *
    * @throws IOException
    */
-  private synchronized void checkDatanodeUuid() throws IOException {
+  synchronized void checkDatanodeUuid() throws IOException {
     if (storage.getDatanodeUuid() == null) {
       storage.setDatanodeUuid(generateUuid());
       storage.writeAll();
@@ -2896,7 +2896,7 @@ public class DataNode extends ReconfigurableBase
   }
 
   public String getDatanodeUuid() {
-    return id == null ? null : id.getDatanodeUuid();
+    return storage == null ? null : storage.getDatanodeUuid();
   }
 
   boolean shouldRun() {
