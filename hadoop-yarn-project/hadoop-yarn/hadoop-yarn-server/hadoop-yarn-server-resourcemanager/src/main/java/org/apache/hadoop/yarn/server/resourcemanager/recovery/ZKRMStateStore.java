659748ea63a05fc76f3c552dfdc840d12ec25a11
Jian He <jianhe@apache.org>
2015-05-08 15:10:38 -0700

    YARN-3604. Fixed ZKRMStateStore#removeApplication to also disable watch. Contributed Zhihai Xu (cherry picked from commit d0b73287c26983f32b0efe01862150e9fd28086f)
    (cherry picked from commit 5a1c2fed349fd3e8a53300f574a952f0c3f622b6)
    
    Change-Id: I274f41e999c80807e6030e117553e7d83513d453

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/recovery/ZKRMStateStore.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/recovery/ZKRMStateStore.java
@@ -834,8 +834,8 @@ public class ZKRMStateStore extends RMStateStore {
   public synchronized void removeApplication(ApplicationId removeAppId)
       throws Exception {
     String appIdRemovePath = getNodePath(rmAppRoot, removeAppId.toString());
-    if (existsWithRetries(appIdRemovePath, true) != null) {
-      deleteWithRetries(appIdRemovePath, true);
+    if (existsWithRetries(appIdRemovePath, false) != null) {
+      deleteWithRetries(appIdRemovePath, false);
     }
   }
 
