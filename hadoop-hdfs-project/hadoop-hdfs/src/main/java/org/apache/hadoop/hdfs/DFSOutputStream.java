27abae6c1aac9f9ccbf89265f2ca0bce6f54b5f3
Vinayakumar B <vinayakumarb@apache.org>
2015-05-08 17:18:14 +0530

    HDFS-3384. DataStreamer thread should be closed immediatly when failed to setup a PipelineForAppendOrRecovery (Contributed by Uma Maheswara Rao G)
    (cherry picked from commit c648317a68891e1c900f04b7a9c98ba40c5faddb)
    (cherry picked from commit d129bbbb37eadc0d096c0ca5f2c86cb57e148b75)
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/CHANGES.txt
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DataStreamer.java
    	hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/TestFileAppend.java
    
    Change-Id: Iaaa79f2d329fdb387a71857d51e33bb5afed0930
    (cherry picked from commit b9c1d79f50decbf1f44c210ae54831e49f6bec4f)
    (cherry picked from commit 451d76f8ed9aa4bd788c79d52b7f719ba3007467)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSOutputStream.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSOutputStream.java
@@ -678,6 +678,9 @@ public class DFSOutputStream extends FSOutputSummer
               DFSClient.LOG.debug("Append to block " + block);
             }
             setupPipelineForAppendOrRecovery();
+            if (true == streamerClosed) {
+              continue;
+            }
             initDataStreaming();
           }
 
@@ -793,6 +796,7 @@ public class DFSOutputStream extends FSOutputSummer
           } else {
             setLastException(new IOException("DataStreamer Exception: ",e));
           }
+          assert !(e instanceof NullPointerException);
           hasError = true;
           if (errorIndex == -1 && restartingNodeIndex == -1) {
             // Not a datanode issue
