7243c228d4464abb35cc1dfa6f45174b5af2eb38
Lei Xu <lei@cloudera.com>
2015-06-01 13:32:13 -0700

    CLOUDERA-BUILD: Enables the shell warnings by default. (CDH-28256)
    (cherry picked from commit a57cde46e270ced269e21d80c35125907506140b)
    (cherry picked from commit 4ecbcabe5b948b18ebd0b2c4fb62b5a6c65aa1b7)
    (cherry picked from commit d66141bb25c0d5ea5e8f228db8a7ea4171913275)
    (cherry picked from commit c7777585e79462cffe49cb1d500f23c2f86e3d1b)

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/CommonConfigurationKeysPublic.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/CommonConfigurationKeysPublic.java
@@ -784,8 +784,9 @@ public class CommonConfigurationKeysPublic {
    */
   public static final String HADOOP_SHELL_MISSING_DEFAULT_FS_WARNING_KEY =
       "hadoop.shell.missing.defaultFs.warning";
+  // CLOUDERA-BUILD: enable the shell warnings by default.
   public static final boolean HADOOP_SHELL_MISSING_DEFAULT_FS_WARNING_DEFAULT =
-      false;
+      true;
 
   /**
    * @see
