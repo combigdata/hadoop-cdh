6e69bc1ebd9e1c7a888682e3c576aab4e2dd18c7
Wei-Chiu Chuang <weichiu@cloudera.com>
2015-12-21 08:18:57 -0800

    HDFS-6673. Add delimited format support to PB OIV tool. Contributed by Eddy Xu.
    (cherry picked from commit caf7298e49f646a40023af999f62d61846fde5e2)
    (cherry picked from commit 55bb4eca7350c780085f52d15098ef559352c840)
    
    Conflicts:
      hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/tools/offlineImageViewer/OfflineImageViewerPB.java
      hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/tools/offlineImageViewer/TestOfflineImageViewer.java
    
    (cherry picked from commit 7b74019c4d951cc7f6152ddbce96df74ec924074)
    Changed diamond operator and try-with-resources to be compatible with JDK 1.6.
    
    Change-Id: Idca80439ecdc8991b6a6a2e706f18e397261551e

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/tools/offlineImageViewer/FSImageLoader.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/tools/offlineImageViewer/FSImageLoader.java
@@ -240,7 +240,7 @@ class FSImageLoader {
     return inodes;
   }
 
-  private static String[] loadStringTable(InputStream in) throws
+  static String[] loadStringTable(InputStream in) throws
   IOException {
     FsImageProto.StringTableSection s = FsImageProto.StringTableSection
         .parseDelimitedFrom(in);
@@ -494,7 +494,7 @@ class FSImageLoader {
     }
   }
 
-  private long getFileSize(FsImageProto.INodeSection.INodeFile f) {
+  static long getFileSize(FsImageProto.INodeSection.INodeFile f) {
     long size = 0;
     for (HdfsProtos.BlockProto p : f.getBlocksList()) {
       size += p.getNumBytes();
