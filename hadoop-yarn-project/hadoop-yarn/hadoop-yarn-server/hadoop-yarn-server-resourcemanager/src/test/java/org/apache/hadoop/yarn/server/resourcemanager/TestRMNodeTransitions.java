601c00ba1f16ebbeec0795a73e3f61a6d3e345d3
Daniel Templeton <daniel@cloudera.com>
2016-02-26 20:28:29 +0000

    YARN-4723. NodesListManager$UnknownNodeId ClassCastException. Contributed by Kuhu Shukla
    (cherry picked from commit f68e1401b8936f49f69c4bf9b66e8f3c8c54991b)
    
    Conflicts:
    	hadoop-yarn-project/CHANGES.txt
    
    Change-Id: I40bb66eefd8b66cfd0bfdf6fdafd3034c0796c33
    (cherry picked from commit eb30b9e6ed06c0f8eb9200b9730f728125d0ff9f)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/TestRMNodeTransitions.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/TestRMNodeTransitions.java
@@ -498,6 +498,20 @@ public class TestRMNodeTransitions {
     Assert.assertEquals(finishedAppId, hbrsp.getApplicationsToCleanup().get(0));
   }
 
+  @Test
+  public void testUnknownNodeId() {
+    NodesListManager.UnknownNodeId nodeId =
+        new NodesListManager.UnknownNodeId("host1");
+    RMNodeImpl node =
+        new RMNodeImpl(nodeId, rmContext, null, 0, 0, null, null, null);
+    rmContext.getInactiveRMNodes().putIfAbsent(nodeId, node);
+    node.handle(
+        new RMNodeEvent(node.getNodeID(), RMNodeEventType.DECOMMISSION));
+    Assert.assertNull(
+        "Must be null as there is no NODE_UNUSABLE update",
+        nodesListManagerEvent);
+  }
+
   private RMNodeImpl getRunningNode() {
     return getRunningNode(null, 0);
   }
