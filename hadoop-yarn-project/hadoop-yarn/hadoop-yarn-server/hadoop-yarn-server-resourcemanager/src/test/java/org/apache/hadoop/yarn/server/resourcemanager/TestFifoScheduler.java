2b16cefa69a1e73ae5633a9f534c999c2a365111
Eric Payne <epayne@apache.org>
2016-04-21 21:15:12 +0000

    YARN-4556. TestFifoScheduler.testResourceOverCommit fails. Contributed by Akihiro Suda (cherry picked from commit 3dce486d88895dcdf443f4d0064d1fb6e9116045)
    (cherry picked from commit a7f903b2ba79c595ce19549b68597d0040abc877)
    
    In CDH, we have two TestFifoScheduler.java files. Upstream, they have been merged into one. Pulling that change in requires pulling in many other unrelated ones. So, manually ported the fix to the corresponding test in CDH.
    
    Conflicts:
    	hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fifo/TestFifoScheduler.java
    
    Change-Id: Icb6b388232597fc483921f753b5f82e5b8208552
    (cherry picked from commit 8a8606e71ed92b0ebe6655df9d208ceb097581c2)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/TestFifoScheduler.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/TestFifoScheduler.java
@@ -527,6 +527,7 @@ public class TestFifoScheduler {
   
   @Test
   public void testResourceOverCommit() throws Exception {
+    int waitCount;
     MockRM rm = new MockRM(conf);
     rm.start();
     
@@ -579,6 +580,17 @@ public class TestFifoScheduler {
         UpdateNodeResourceRequest.newInstance(nodeResourceMap);
     AdminService as = rm.adminService;
     as.updateNodeResource(request);
+
+    waitCount = 0;
+    while (waitCount++ != 20) {
+      report_nm1 = rm.getResourceScheduler().getNodeReport(nm1.getNodeId());
+      if (report_nm1.getAvailableResource().getMemory() != 0) {
+        break;
+      }
+      LOG.info("Waiting for RMNodeResourceUpdateEvent to be handled... Tried "
+          + waitCount + " times already..");
+      Thread.sleep(1000);
+    }
     
     // Now, the used resource is still 4 GB, and available resource is minus value.
     report_nm1 = rm.getResourceScheduler().getNodeReport(nm1.getNodeId());
@@ -589,7 +601,7 @@ public class TestFifoScheduler {
     ContainerStatus containerStatus = BuilderUtils.newContainerStatus(
         c1.getId(), ContainerState.COMPLETE, "", 0);
     nm1.containerStatus(containerStatus);
-    int waitCount = 0;
+    waitCount = 0;
     while (attempt1.getJustFinishedContainers().size() < 1
         && waitCount++ != 20) {
       LOG.info("Waiting for containers to be finished for app 1... Tried "
