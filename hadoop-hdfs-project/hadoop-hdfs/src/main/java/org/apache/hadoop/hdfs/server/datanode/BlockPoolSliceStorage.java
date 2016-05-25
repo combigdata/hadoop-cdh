905feaca55a11bc9f5d466d8fa58c14119040911
Wei-Chiu Chuang <weichiu@cloudera.com>
2016-05-25 15:18:24 -0700

    CLOUDERA-BUILD. CDH-40786 Addendum patch for HDFS-10360 (DataNode may format directory and lose blocks if current/VERSION is missing)
    Change-Id: Ie7b6a52530866a1455626875c34ea7e3b3314ec8
    (cherry picked from commit 1d9ac57c8318a72b264eabf9d84f42097ab8c438)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/BlockPoolSliceStorage.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/BlockPoolSliceStorage.java
@@ -147,7 +147,7 @@ public class BlockPoolSliceStorage extends Storage {
       NamespaceInfo nsInfo, File dataDir, StartupOption startOpt) throws IOException {
     StorageDirectory sd = new StorageDirectory(dataDir, null, true);
     try {
-      StorageState curState = sd.analyzeStorage(startOpt, this);
+      StorageState curState = sd.analyzeStorage(startOpt, this, true);
       // sd is locked but not opened
       switch (curState) {
       case NORMAL:
