a69e45fb0ea25a84c1e44b862a6ab397cdf04b0f
Zhe Zhang <zhz@apache.org>
2016-06-06 15:52:39 -0700

    HDFS-10458. getFileEncryptionInfo should return quickly for non-encrypted cluster.
    (cherry picked from commit 6de9213df111a9a4ed875db995d67af72d08a798)
    (cherry picked from commit 06e38c835d0ad9619d4bea662f2dd7d0f62007a9)
    (cherry picked from commit 0274636529cfac4d64582a7c21631aebbf4deb1b)
    (cherry picked from commit 9b68eda2746702246c7f4b4abe412c974ba56339)
    
     Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/EncryptionZoneManager.java
    
    Change-Id: Ic06877632b7a1b17288f86ae93f6a454871892c8

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSDirectory.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSDirectory.java
@@ -2945,7 +2945,7 @@ public class FSDirectory implements Closeable {
    */
   FileEncryptionInfo getFileEncryptionInfo(INode inode, int snapshotId,
       INodesInPath iip) throws IOException {
-    if (!inode.isFile()) {
+    if (!inode.isFile() || !ezManager.hasCreatedEncryptionZone()) {
       return null;
     }
     readLock();
