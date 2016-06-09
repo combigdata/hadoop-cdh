0a61119508a7e02132eb3daa7cd8993002ba3582
Steve Loughran <stevel@apache.org>
2016-06-09 20:58:30 +0100

    HADOOP-12537 S3A to support Amazon STS temporary credentials. Contributed by Sean Mackrory.
    (cherry-picked from commit 31ffaf76f2b6e1fd2a141daa4daaebdfecefe727)
    
    Conflicts:
    	hadoop-project/pom.xml
    	hadoop-tools/hadoop-aws/src/site/markdown/tools/hadoop-aws/index.md
    
    Change-Id: Id69f75366129b152669caaa19532a1c2732081ab
    (cherry picked from commit 2e0c60a92febc8c27b66bcd7dd7e37581ce9b965)

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/TemporaryAWSCredentialsProvider.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/TemporaryAWSCredentialsProvider.java
@@ -19,27 +19,47 @@
 package org.apache.hadoop.fs.s3a;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.auth.AWSCredentials;
 import org.apache.commons.lang.StringUtils;
 
-public class BasicAWSCredentialsProvider implements AWSCredentialsProvider {
+import java.net.URI;
+
+import org.apache.hadoop.classification.InterfaceAudience;
+import org.apache.hadoop.classification.InterfaceStability;
+import org.apache.hadoop.conf.Configuration;
+
+import static org.apache.hadoop.fs.s3a.Constants.*;
+
+/**
+ * Support session credentials for authenticating with AWS.
+ */
+@InterfaceAudience.Private
+@InterfaceStability.Stable
+public class TemporaryAWSCredentialsProvider implements AWSCredentialsProvider {
+
+  public static final String NAME
+      = "org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider";
   private final String accessKey;
   private final String secretKey;
+  private final String sessionToken;
 
-  public BasicAWSCredentialsProvider(String accessKey, String secretKey) {
-    this.accessKey = accessKey;
-    this.secretKey = secretKey;
+  public TemporaryAWSCredentialsProvider(URI uri, Configuration conf) {
+    this.accessKey = conf.get(ACCESS_KEY, null);
+    this.secretKey = conf.get(SECRET_KEY, null);
+    this.sessionToken = conf.get(SESSION_TOKEN, null);
   }
 
   public AWSCredentials getCredentials() {
-    if (!StringUtils.isEmpty(accessKey) && !StringUtils.isEmpty(secretKey)) {
-      return new BasicAWSCredentials(accessKey, secretKey);
+    if (!StringUtils.isEmpty(accessKey) && !StringUtils.isEmpty(secretKey)
+        && !StringUtils.isEmpty(sessionToken)) {
+      return new BasicSessionCredentials(accessKey, secretKey, sessionToken);
     }
     throw new CredentialInitializationException(
-        "Access key or secret key is null");
+        "Access key, secret key or session token is unset");
   }
 
+  @Override
   public void refresh() {}
 
   @Override
