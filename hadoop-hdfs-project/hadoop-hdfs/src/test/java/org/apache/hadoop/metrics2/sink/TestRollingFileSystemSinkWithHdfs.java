a03d7bbfca5aa9024237e1529bad7c9e32bb7b48
Daniel Templeton <daniel@cloudera.com>
2016-06-29 10:22:04 -0700

    Revert "HDFS-10463. TestRollingFileSystemSinkWithHdfs needs some cleanup. (Daniel Templeton via kasha)"
    This reverts commit 18ffa4b544e8ac46a1bfc62028002d2ea893bb52.
    
    Change-Id: I42c6ce64c43d4e704366d06b89d974ebbfbacf17

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/metrics2/sink/TestRollingFileSystemSinkWithHdfs.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/metrics2/sink/TestRollingFileSystemSinkWithHdfs.java
@@ -109,7 +109,7 @@ public class TestRollingFileSystemSinkWithHdfs
   public void testSilentAppend() throws Exception {
     String path = "hdfs://" + cluster.getNameNode().getHostAndPort() + "/tmp";
 
-    assertExtraContents(doAppendTest(path, true, true, 1));
+    assertExtraContents(doAppendTest(path, false, true, 1));
   }
 
   /**
@@ -159,11 +159,8 @@ public class TestRollingFileSystemSinkWithHdfs
     assertTrue("No exception was generated while writing metrics "
         + "even though HDFS was unavailable", MockSink.errored);
 
-    try {
-      ms.stop();
-    } finally {
-      ms.shutdown();
-    }
+    ms.stop();
+    ms.shutdown();
   }
 
   /**
@@ -186,14 +183,13 @@ public class TestRollingFileSystemSinkWithHdfs
 
     try {
       ms.stop();
-
       fail("No exception was generated while stopping sink "
           + "even though HDFS was unavailable");
     } catch (MetricsException ex) {
       // Expected
-    } finally {
-      ms.shutdown();
     }
+
+    ms.shutdown();
   }
 
   /**
@@ -219,11 +215,8 @@ public class TestRollingFileSystemSinkWithHdfs
         + "while HDFS was unavailable, even though the sink is set to "
         + "ignore errors", MockSink.errored);
 
-    try {
-      ms.stop();
-    } finally {
-      ms.shutdown();
-    }
+    ms.stop();
+    ms.shutdown();
   }
 
   /**
@@ -244,15 +237,13 @@ public class TestRollingFileSystemSinkWithHdfs
     shutdownHdfs();
     MockSink.errored = false;
 
-    try {
-      ms.stop();
+    ms.stop();
 
-      assertFalse("An exception was generated stopping sink "
-          + "while HDFS was unavailable, even though the sink is set to "
-          + "ignore errors", MockSink.errored);
-    } finally {
-      ms.shutdown();
-    }
+    assertFalse("An exception was generated stopping sink "
+        + "while HDFS was unavailable, even though the sink is set to "
+        + "ignore errors", MockSink.errored);
+
+    ms.shutdown();
   }
 
   /**
@@ -281,37 +272,29 @@ public class TestRollingFileSystemSinkWithHdfs
 
     int count = 0;
 
-    try {
-      // Sleep until the flusher has run. This should never actually need to
-      // sleep, but the sleep is here to make sure this test isn't flakey.
-      while (!RollingFileSystemSink.hasFlushed) {
-        Thread.sleep(10L);
+    // Sleep until the flusher has run. This should never actually need to
+    // sleep, but the sleep is here to make sure this test isn't flakey.
+    while (!RollingFileSystemSink.hasFlushed) {
+      Thread.sleep(10L);
 
-        if (++count > 1000) {
-          fail("Flush thread did not run within 10 seconds");
-        }
-      }
-
-      Calendar now = Calendar.getInstance();
-      Path currentDir = new Path(path, DATE_FORMAT.format(now.getTime()) + "00");
-      FileSystem fs = FileSystem.newInstance(new URI(path), new Configuration());
-      Path currentFile =
-          findMostRecentLogFile(fs, new Path(currentDir, getLogFilename()));
-      FileStatus status = fs.getFileStatus(currentFile);
-
-      // Each metrics record is 118+ bytes, depending on hostname
-      assertTrue("The flusher thread didn't flush the log contents. Expected "
-          + "at least 236 bytes in the log file, but got " + status.getLen(),
-          status.getLen() >= 236);
-    } finally {
-      RollingFileSystemSink.forceFlush = false;
-
-      try {
-        ms.stop();
-      } finally {
-        ms.shutdown();
+      if (++count > 1000) {
+        fail("Flush thread did not run within 10 seconds");
       }
     }
+
+    Calendar now = Calendar.getInstance();
+    Path currentDir = new Path(path, DATE_FORMAT.format(now.getTime()) + "00");
+    FileSystem fs = FileSystem.newInstance(new URI(path), new Configuration());
+    Path currentFile =
+        findMostRecentLogFile(fs, new Path(currentDir, getLogFilename()));
+    FileStatus status = fs.getFileStatus(currentFile);
+
+    // Each metrics record is 118+ bytes, depending on hostname
+    assertTrue("The flusher thread didn't flush the log contents. Expected "
+        + "at least 236 bytes in the log file, but got " + status.getLen(),
+        status.getLen() >= 236);
+
+    ms.stop();
   }
 
   /**
