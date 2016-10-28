apache/hadoop

HDFS-9290. DFSClient#callAppend() is not backward compatible for slightly older NameNodes. Contributed by Tony Wu. (cherry picked from commit b9e0417bdf2b9655dc4256bdb43683eca1ab46be)

hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DFSClient.java
@@ -1364,8 +1364,14 @@ public class DFSClient implements java.io.Closeable, RemotePeerFactory,
     try {
       LastBlockWithStatus blkWithStatus = namenode.append(src, clientName,
           new EnumSetWritable<>(flag, CreateFlag.class));
+      HdfsFileStatus status = blkWithStatus.getFileStatus();
+      if (status == null) {
+        DFSClient.LOG.debug("NameNode is on an older version, request file " +
+            "info with additional RPC call for file: " + src);
+        status = getFileInfo(src);
+      }
       return DFSOutputStream.newStreamForAppend(this, src, flag, progress,
-          blkWithStatus.getLastBlock(), blkWithStatus.getFileStatus(),
+          blkWithStatus.getLastBlock(), status,
           dfsClientConf.createChecksum(null), favoredNodes);
     } catch(RemoteException re) {
       throw re.unwrapRemoteException(AccessControlException.class,

combigdata/hadoop-cdh

Change-Id: Ie6520440322e4c1bd2b438ec37c9f0afbaa4c3bd
(cherry picked from commit be5551c3c937513eef78edbb045b0b17df474705)
(cherry picked from commit 3be1c8d0c4fd516acd0576bbb326da6f50b6b1d7)

hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSClient.java
@@ -1872,6 +1872,11 @@ public class DFSClient implements java.io.Closeable, RemotePeerFactory,
                                      SnapshotAccessControlException.class);
     }
     HdfsFileStatus newStat = lastBlockWithStatus.getFileStatus();
+    if (newStat == null) {
+      DFSClient.LOG.debug("NameNode is on an older version, request file " +
+          "info with additional RPC call for file: " + src);
+      newStat = getFileInfo(src);
+    }
     return DFSOutputStream.newStreamForAppend(this, src, buffersize, progress,
         lastBlockWithStatus.getLastBlock(), newStat,
         dfsClientConf.createChecksum());