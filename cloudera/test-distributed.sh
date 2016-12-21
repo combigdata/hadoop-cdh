a8212aca730f876336b7d83a25f781a71e492c97
Wei-Chiu Chuang <weichiu@cloudera.com>
2016-12-21 10:31:27 -0800

    CLOUDERA-BUILD. Use dist_test for precommit check.
    (cherry picked from commit 5ec1e1620446a190beb9de7ce2546aaa1f63b7b5)
    
    Conflicts:
      cloudera/test-distributed.sh
    
    Change-Id: Ie74a8cf6c59c20f252cf297237acea9b8a831f85

--- cloudera/test-distributed.sh
+++ cloudera/test-distributed.sh
@@ -7,27 +7,23 @@ cd $DIR
 # Build the project
 $DIR/build.sh
 
-# Install supertest locally
-SCRIPTS="supertest"
+# Install dist_test locally
+SCRIPTS="dist_test"
 
 if [[ -d $SCRIPTS ]]; then
     echo "Cleaning up remnants from a previous run"
     rm -rf $SCRIPTS
 fi
 
-git clone --depth 1 git://github.mtv.cloudera.com/CDH/$SCRIPTS.git $SCRIPTS || true
+git clone --depth 1 https://github.com/cloudera/$SCRIPTS.git $SCRIPTS || true
 
 # Fetch the right branch
 cd "$DIR/$SCRIPTS"
 git fetch --depth 1 origin
 git checkout -f origin/master
 git ls-tree -r HEAD
-./init.sh
-git submodule status
-./make-config.sh
-# Activate the supertest virtualenv
-source ./supertest-env/bin/activate
-export PATH=`pwd`/grind/bin/:$PATH
+./setup.sh
+export PATH=`pwd`/bin/:$PATH
 which grind
 
 if [[ -z $DIST_TEST_USER || -z $DIST_TEST_PASSWORD ]]; then
@@ -49,9 +45,9 @@ artifact_archive_globs = ["**/surefire-reports/TEST-*.xml"]
 EOF
 
 # Invoke grind to run tests
-grind -c ${DIR}/supertest/grind.cfg config
-grind -c ${DIR}/supertest/grind.cfg pconfig
-grind -c ${DIR}/supertest/grind.cfg test --artifacts -r 3 -e TestRM -e TestWorkPreservingRMRestart -e TestRMRestart -e TestContainerAllocation -e TestMRJobClient -e TestCapacityScheduler -e TestDelegatingInputFormat -e TestMRCJCFileInputFormat -e TestJobHistoryEventHandler -e TestCombineFileInputFormat -e TestAMRMRPCResponseId -e TestSystemMetricsPublisher -e TestNodesListManager -e TestRMContainerImpl -e TestApplicationMasterLauncher -e TestRMWebApp -e TestContainerManagerSecurity -e TestResourceManager -e TestParameterParser -e TestNativeCodeLoader -e TestRMContainerAllocator -e TestMRIntermediateDataEncryption -e TestWebApp -e TestCryptoStreamsWithOpensslAesCtrCryptoCodec -e TestDNS -e TestClientRMTokens -e TestAMAuthorization -e TestContinuousScheduling
+grind -c ${DIR}/$SCRIPTS/env/grind.cfg config
+grind -c ${DIR}/$SCRIPTS/env/grind.cfg pconfig
+grind -c ${DIR}/$SCRIPTS/env/grind.cfg test --artifacts -r 3 -e TestRM -e TestWorkPreservingRMRestart -e TestRMRestart -e TestContainerAllocation -e TestMRJobClient -e TestCapacityScheduler -e TestDelegatingInputFormat -e TestMRCJCFileInputFormat -e TestJobHistoryEventHandler -e TestCombineFileInputFormat -e TestAMRMRPCResponseId -e TestSystemMetricsPublisher -e TestNodesListManager -e TestRMContainerImpl -e TestApplicationMasterLauncher -e TestRMWebApp -e TestContainerManagerSecurity -e TestResourceManager -e TestParameterParser -e TestNativeCodeLoader -e TestRMContainerAllocator -e TestMRIntermediateDataEncryption -e TestWebApp -e TestCryptoStreamsWithOpensslAesCtrCryptoCodec -e TestDNS -e TestClientRMTokens -e TestAMAuthorization -e TestContinuousScheduling
 # TestDNS fails only on supertest. CDH-37451
 # TestClientRMTokens and TestAMAuthorization to be fixed in 5.8 (CDH-39590)
 # TestContinuousScheduling has been failing consistently, to be fixed in 5.8 (CDH-38830)
