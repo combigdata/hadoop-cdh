ajfabbri/s3guard-hadoop

HADOOP-13651 S3Guard: S3AFileSystem Integration with MetadataStore

hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/s3guard/S3Guard.java
@@ -1,0 +1,331 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.fs.s3a.s3guard;
+
+import com.google.common.base.Preconditions;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.fs.s3a.S3AFileStatus;
+import org.apache.hadoop.fs.s3a.S3AUtils;
+import org.apache.hadoop.util.ReflectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Logic for integrating MetadataStore with S3A.
+ */
+final public class S3Guard {
+  private static final Logger LOG = LoggerFactory.getLogger(S3Guard.class);
+
+  /* Constants. */
+  public static final String S3_METADATA_STORE_IMPL =
+      "fs.s3a.metadatastore.impl";
+
+  // Utility class.  All static functions.
+  private S3Guard() { }
+
+  /* Utility functions. */
+
+  /**
+   * Create a new instance of the configured MetadataStore.
+   * The returned MetadataStore will have been initialized via
+   * {@link MetadataStore#initialize(FileSystem)} by this function before
+   * returning it.  Callers must clean up by calling
+   * {@link MetadataStore#close()} when done using the MetadataStore.
+   *
+   * If this fails to instantiate the class specified in config (fs.s3a
+   * .metadatastore.impl), or there is an error initializing it, this will log
+   * an error and return an instance of {@link NullMetadataStore} instead.
+   *
+   * @param fs  FileSystem whose Configuration specifies which
+   *            implementation to use.
+   * @return Reference to new MetadataStore.
+   */
+  public static MetadataStore getMetadataStore(FileSystem fs) {
+
+    Preconditions.checkNotNull(fs);
+    Configuration conf = fs.getConf();
+    Preconditions.checkNotNull(conf);
+    MetadataStore msInstance;
+    try {
+      Class<? extends MetadataStore> msClass = getMetadataStoreClass(conf);
+      msInstance = ReflectionUtils.newInstance(msClass, conf);
+      LOG.info("Using {} for {} filesystem", msClass.getSimpleName(),
+          fs.getScheme());
+    } catch (RuntimeException e) {
+      LOG.error("Failed to instantiate {}, using NullMetadataStore:",
+          conf.get(S3_METADATA_STORE_IMPL), e);
+      msInstance = new NullMetadataStore();
+    }
+    try {
+      msInstance.initialize(fs);
+    } catch (IOException ioe) {
+      LOG.error("Exception initializing MetadataStore, falling back to " +
+          "NullMetadataStore: ", ioe);
+      msInstance = new NullMetadataStore();
+      // no init needed for NullMetadataStore
+    }
+    return msInstance;
+  }
+
+  /**
+   * @param conf Configuration
+   * @return true if NullMetadataStore is configured for s3a, or if the
+   * configuration is mising.
+   */
+  public static boolean isNullMetadataStoreConfigured(Configuration conf) {
+    Class<? extends MetadataStore> msClass = getMetadataStoreClass(conf);
+    return msClass.equals(NullMetadataStore.class);
+  }
+
+  private static Class<? extends MetadataStore> getMetadataStoreClass(
+      Configuration conf) {
+    if (conf == null) {
+      return NullMetadataStore.class;
+    }
+
+    return conf.getClass(S3_METADATA_STORE_IMPL, NullMetadataStore.class,
+            MetadataStore.class);
+  }
+
+
+  /**
+   * Helper function which puts given S3AFileStatus in the MetadataStore and
+   * returns the same S3AFileStatus.
+   * @param ms MetadataStore to put() into.
+   * @param status status to store
+   * @return The same status as passed in
+   * @throws IOException if metadata store update failed
+   */
+  public static S3AFileStatus putAndReturn(MetadataStore ms,
+      S3AFileStatus status) throws IOException {
+
+    ms.put(new PathMetadata(status));
+    return status;
+  }
+
+  public static FileStatus[] dirMetaToStatuses(DirListingMetadata dirMeta)  {
+    Collection<PathMetadata> listing = dirMeta.getListing();
+    FileStatus[] statuses = new FileStatus[listing.size()];
+
+    // TODO filter out deleted entries
+    int i = 0;
+    for (PathMetadata pm : listing) {
+      statuses[i++] = pm.getFileStatus();
+    }
+
+    return statuses;
+  }
+
+  /**
+   * Given directory listing metadata from both the backing store, and the
+   * MetadataStore, merge the two sources of truth to create a consistent
+   * view of the current directory contents, which can be returned to clients.
+   *
+   * Also update the MetadataStore to reflect the resulting directory listing.
+   *
+   * @param ms MetadataStore to use.
+   * @param path path to directory
+   * @param backingStatuses Directory listing from the backing store.
+   * @param dirMeta  Directory listing from MetadataStore.  May be null.
+   * @return Final result of directory listing.
+   * @throws IOException if metadata store update failed
+   */
+  public static FileStatus[] dirListingUnion(MetadataStore ms, Path path,
+      List<FileStatus> backingStatuses, DirListingMetadata dirMeta)
+      throws IOException {
+
+    // Fast-path for NullMetadataStore
+    if (ms instanceof NullMetadataStore) {
+      return backingStatuses.toArray(new FileStatus[backingStatuses.size()]);
+    }
+
+    assertQualified(path);
+
+    if (dirMeta == null) {
+      // The metadataStore had zero state for this directory
+      dirMeta = new DirListingMetadata(path, DirListingMetadata.EMPTY_DIR,
+          false);
+    }
+
+    // Since we treat the MetadataStore as a "fresher" or "consistent" view
+    // of metadata, we always use its metadata first.
+
+    // Since the authoritative case is already handled outside this function,
+    // we will basically start with the set of directory entries in the
+    // DirListingMetadata, and add any that only exist in the backingStatuses.
+
+    // TODO filter out deleted files via PathMetadata#isDeleted()
+
+    for (FileStatus s : backingStatuses) {
+
+      // Minor race condition here.  Multiple threads could add to this
+      // mutable DirListingMetadata.  Since it is backed by a
+      // ConcurrentHashMap, the last put() wins.  I think this is ok.
+      // More concerning is two threads racing on listStatus() and delete().
+      // This appears to be a real issue, so:
+      // TODO expose an atomic DirListingMetadata#putIfNotPresent()
+      if (dirMeta.get(s.getPath()) == null) {
+        dirMeta.put(s);
+      }
+    }
+
+    dirMeta.setAuthoritative(true); // This is the full directory contents
+    ms.put(dirMeta);
+    return dirMetaToStatuses(dirMeta);
+  }
+
+  /**
+   * Although NullMetadataStore does nothing, callers may wish to avoid work
+   * (fast path) when the NullMetadataStore is in use.
+   * @param ms The MetadataStore to test
+   * @return true iff the MetadataStore is the null, or no-op, implementation.
+   */
+  public static boolean isNullMetadataStore(MetadataStore ms) {
+    return (ms instanceof NullMetadataStore);
+  }
+
+  /**
+   * Update MetadataStore to reflect creation of the given  directories.
+   * @param ms    MetadataStore to update.
+   * @param dirs  null, or an ordered list of directories from leaf to root.
+   *              E.g. if /a/ exists, and  mkdirs(/a/b/c/d) is called, this
+   *              list will contain [/a/b/c/d, /a/b/c, /a/b].   /a/b/c/d is
+   *              an empty, dir, and the other dirs only contain their child
+   *              dir.
+   * @param permission Permissions associated with the directories.
+   *                   Currently ignored by s3a and here as well.
+   */
+  public static void makeDirsOrdered(MetadataStore ms, List<Path> dirs,
+      FsPermission permission) {
+    if (dirs == null) {
+      return;
+    }
+
+    /* We discussed atomicity of this implementation.
+     * The concern is that multiple clients could race to write different
+     * cached directories to the MetadataStore.  Two solutions are proposed:
+     * 1. Move mkdirs() into MetadataStore interface and let implementations
+     *    ensure they are atomic.
+     * 2. Specify that the semantics of MetadataStore#putListStatus() is
+     *    always additive,  That is, if MetadataStore has listStatus() state
+     *    for /a/b that contains [/a/b/file0, /a/b/file1], and we then call
+     *    putListStatus(/a/b -> [/a/b/file2, /a/b/file3], isAuthoritative=true),
+     *    then we will end up with final state of
+     *    [/a/b/file0, /a/b/file1, /a/b/file2, /a/b/file3], isAuthoritative =
+     *    true
+     */
+    S3AFileStatus prevStatus = null;
+    // Iterate from leaf to root
+    for (int i = 0; i < dirs.size(); i++) {
+      boolean isLeaf = (prevStatus == null);
+      Path f = dirs.get(i);
+      assertQualified(f);
+      S3AFileStatus status = new S3AFileStatus(true, isLeaf, f);
+      Collection<PathMetadata> children;
+      if (isLeaf) {
+        children = DirListingMetadata.EMPTY_DIR;
+      } else {
+        children = new ArrayList<>(1);
+        children.add(new PathMetadata(prevStatus));
+      }
+      DirListingMetadata dirMeta = new DirListingMetadata(f, children, true);
+      try {
+        ms.put(dirMeta);
+        ms.put(new PathMetadata(status));
+      } catch (IOException ioe) {
+        LOG.error("MetadataStore#put() failure:", ioe);
+        return;
+      }
+      prevStatus = status;
+    }
+  }
+
+  /**
+   * Helper function that records the move of directory path, adding
+   * resulting metadata to the supplied lists.  Does not store in MetadataStore.
+   * @param ms  MetadataStore, used to make this a no-op, when it is
+   *            NullMetadataStore.
+   * @param srcPaths stores the source path here
+   * @param dstMetas stores destination metadata here
+   * @param srcPath  source path to store
+   * @param dstPath  destination path to store
+   */
+  public static void addMoveDir(MetadataStore ms, Collection<Path> srcPaths,
+      Collection<PathMetadata> dstMetas, Path srcPath, Path dstPath) {
+    if (isNullMetadataStore(ms)) {
+      return;
+    }
+    assertQualified(srcPath);
+    assertQualified(dstPath);
+    S3AFileStatus dstStatus = new S3AFileStatus(true, true, dstPath);
+    addMoveStatus(srcPaths, dstMetas, srcPath, dstStatus);
+  }
+
+  /**
+   * Like {@link #addMoveDir(MetadataStore, Collection, Collection, Path,
+   * Path)} (), but for files.
+   * @param ms  MetadataStore, used to make this a no-op, when it is
+   *            NullMetadataStore.
+   * @param srcPaths stores the source path here
+   * @param dstMetas stores destination metadata here
+   * @param srcPath  source path to store
+   * @param dstPath  destination path to store
+   * @param size length of file moved
+   * @param blockSize  blocksize to associate with destination file
+   */
+  public static void addMoveFile(MetadataStore ms, Collection<Path> srcPaths,
+      Collection<PathMetadata> dstMetas, Path srcPath, Path dstPath,
+      long size, long blockSize) {
+    if (isNullMetadataStore(ms)) {
+      return;
+    }
+    assertQualified(srcPath);
+    assertQualified(dstPath);
+    S3AFileStatus dstStatus = S3AUtils.createUploadFileStatus(dstPath, false,
+        size, blockSize);
+    addMoveStatus(srcPaths, dstMetas, srcPath, dstStatus);
+  }
+
+  private static void addMoveStatus(Collection<Path> srcPaths,
+      Collection<PathMetadata> dstMetas, Path srcPath, S3AFileStatus dstStatus)
+  {
+    srcPaths.add(srcPath);
+    dstMetas.add(new PathMetadata(dstStatus));
+  }
+
+  public static void assertQualified(Path p) {
+    URI uri = p.toUri();
+    // Paths must include bucket in case MetadataStore is shared between
+    // multiple S3AFileSystem instances
+    Preconditions.checkNotNull(uri.getHost());
+
+    // I believe this should never fail, since there is a host?
+    Preconditions.checkNotNull(uri.getScheme());
+  }
+}

combigdata/hadoop-cdh

(cherry picked from commit f4c37a5794d01b8cc72d747ab465787902436961)
Conflicts:
        hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java
Change-Id: I353563f6976647404a5c7f5aba6a1cf474c96037

hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/s3guard/S3Guard.java
@@ -1,0 +1,334 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.fs.s3a.s3guard;
+
+import com.google.common.base.Preconditions;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.s3a.S3AFileStatus;
+import org.apache.hadoop.fs.s3a.S3AUtils;
+import org.apache.hadoop.util.ReflectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Logic for integrating MetadataStore with S3A.
+ */
+final public class S3Guard {
+  private static final Logger LOG = LoggerFactory.getLogger(S3Guard.class);
+
+  /* Constants. */
+  public static final String S3_METADATA_STORE_IMPL =
+      "fs.s3a.metadatastore.impl";
+
+  // Utility class.  All static functions.
+  private S3Guard() { }
+
+  /* Utility functions. */
+
+  /**
+   * Create a new instance of the configured MetadataStore.
+   * The returned MetadataStore will have been initialized via
+   * {@link MetadataStore#initialize(FileSystem)} by this function before
+   * returning it.  Callers must clean up by calling
+   * {@link MetadataStore#close()} when done using the MetadataStore.
+   *
+   * If this fails to instantiate the class specified in config (fs.s3a
+   * .metadatastore.impl), or there is an error initializing it, this will log
+   * an error and return an instance of {@link NullMetadataStore} instead.
+   *
+   * @param fs  FileSystem whose Configuration specifies which
+   *            implementation to use.
+   * @return Reference to new MetadataStore.
+   */
+  public static MetadataStore getMetadataStore(FileSystem fs) {
+
+    Preconditions.checkNotNull(fs);
+    Configuration conf = fs.getConf();
+    Preconditions.checkNotNull(conf);
+    MetadataStore msInstance;
+    try {
+      Class<? extends MetadataStore> msClass = getMetadataStoreClass(conf);
+      msInstance = ReflectionUtils.newInstance(msClass, conf);
+      LOG.info("Using {} for {} filesystem", msClass.getSimpleName(),
+          fs.getScheme());
+    } catch (RuntimeException e) {
+      LOG.error("Failed to instantiate {}, using NullMetadataStore:",
+          conf.get(S3_METADATA_STORE_IMPL), e);
+      msInstance = new NullMetadataStore();
+    }
+    try {
+      msInstance.initialize(fs);
+    } catch (IOException ioe) {
+      LOG.error("Exception initializing MetadataStore, falling back to " +
+          "NullMetadataStore: ", ioe);
+      msInstance = new NullMetadataStore();
+      // no init needed for NullMetadataStore
+    }
+    return msInstance;
+  }
+
+  /**
+   * @param conf Configuration
+   * @return true if NullMetadataStore is configured for s3a, or if the
+   * configuration is mising.
+   */
+  public static boolean isNullMetadataStoreConfigured(Configuration conf) {
+    Class<? extends MetadataStore> msClass = getMetadataStoreClass(conf);
+    return msClass.equals(NullMetadataStore.class);
+  }
+
+  private static Class<? extends MetadataStore> getMetadataStoreClass(
+      Configuration conf) {
+    if (conf == null) {
+      return NullMetadataStore.class;
+    }
+
+    return conf.getClass(S3_METADATA_STORE_IMPL, NullMetadataStore.class,
+            MetadataStore.class);
+  }
+
+
+  /**
+   * Helper function which puts given S3AFileStatus in the MetadataStore and
+   * returns the same S3AFileStatus.
+   * @param ms MetadataStore to put() into.
+   * @param status status to store
+   * @return The same status as passed in
+   * @throws IOException if metadata store update failed
+   */
+  public static S3AFileStatus putAndReturn(MetadataStore ms,
+      S3AFileStatus status) throws IOException {
+
+    ms.put(new PathMetadata(status));
+    return status;
+  }
+
+  public static FileStatus[] dirMetaToStatuses(DirListingMetadata dirMeta)  {
+    Collection<PathMetadata> listing = dirMeta.getListing();
+    FileStatus[] statuses = new FileStatus[listing.size()];
+
+    // HADOOP-13760: filter out deleted entries here
+    int i = 0;
+    for (PathMetadata pm : listing) {
+      statuses[i++] = pm.getFileStatus();
+    }
+
+    return statuses;
+  }
+
+  /**
+   * Given directory listing metadata from both the backing store, and the
+   * MetadataStore, merge the two sources of truth to create a consistent
+   * view of the current directory contents, which can be returned to clients.
+   *
+   * Also update the MetadataStore to reflect the resulting directory listing.
+   *
+   * @param ms MetadataStore to use.
+   * @param path path to directory
+   * @param backingStatuses Directory listing from the backing store.
+   * @param dirMeta  Directory listing from MetadataStore.  May be null.
+   * @return Final result of directory listing.
+   * @throws IOException if metadata store update failed
+   */
+  public static FileStatus[] dirListingUnion(MetadataStore ms, Path path,
+      List<FileStatus> backingStatuses, DirListingMetadata dirMeta)
+      throws IOException {
+
+    // Fast-path for NullMetadataStore
+    if (ms instanceof NullMetadataStore) {
+      return backingStatuses.toArray(new FileStatus[backingStatuses.size()]);
+    }
+
+    assertQualified(path);
+
+    if (dirMeta == null) {
+      // The metadataStore had zero state for this directory
+      dirMeta = new DirListingMetadata(path, DirListingMetadata.EMPTY_DIR,
+          false);
+    }
+
+    // Since we treat the MetadataStore as a "fresher" or "consistent" view
+    // of metadata, we always use its metadata first.
+
+    // Since the authoritative case is already handled outside this function,
+    // we will basically start with the set of directory entries in the
+    // DirListingMetadata, and add any that only exist in the backingStatuses.
+
+    // HADOOP-13760: filter out deleted files via PathMetadata#isDeleted() here
+
+    for (FileStatus s : backingStatuses) {
+
+      // Minor race condition here.  Multiple threads could add to this
+      // mutable DirListingMetadata.  Since it is backed by a
+      // ConcurrentHashMap, the last put() wins.  I think this is ok.
+      // More concerning is two threads racing on listStatus() and delete().
+      // Any FileSystem has similar race conditions, but we could persist
+      // a stale entry longer.  We could expose an atomic
+      // DirListingMetadata#putIfNotPresent()
+      if (dirMeta.get(s.getPath()) == null) {
+        dirMeta.put(s);
+      }
+    }
+
+    // TODO optimize for when allowAuthoritative = false
+    dirMeta.setAuthoritative(true); // This is the full directory contents
+    ms.put(dirMeta);
+    return dirMetaToStatuses(dirMeta);
+  }
+
+  /**
+   * Although NullMetadataStore does nothing, callers may wish to avoid work
+   * (fast path) when the NullMetadataStore is in use.
+   * @param ms The MetadataStore to test
+   * @return true iff the MetadataStore is the null, or no-op, implementation.
+   */
+  public static boolean isNullMetadataStore(MetadataStore ms) {
+    return (ms instanceof NullMetadataStore);
+  }
+
+  /**
+   * Update MetadataStore to reflect creation of the given  directories.
+   * @param ms    MetadataStore to update.
+   * @param dirs  null, or an ordered list of directories from leaf to root.
+   *              E.g. if /a/ exists, and  mkdirs(/a/b/c/d) is called, this
+   *              list will contain [/a/b/c/d, /a/b/c, /a/b].   /a/b/c/d is
+   *              an empty, dir, and the other dirs only contain their child
+   *              dir.
+   * @param owner Hadoop user name
+   */
+  public static void makeDirsOrdered(MetadataStore ms, List<Path> dirs,
+      String owner) {
+    if (dirs == null) {
+      return;
+    }
+
+    /* We discussed atomicity of this implementation.
+     * The concern is that multiple clients could race to write different
+     * cached directories to the MetadataStore.  Two solutions are proposed:
+     * 1. Move mkdirs() into MetadataStore interface and let implementations
+     *    ensure they are atomic.
+     * 2. Specify that the semantics of MetadataStore#putListStatus() is
+     *    always additive,  That is, if MetadataStore has listStatus() state
+     *    for /a/b that contains [/a/b/file0, /a/b/file1], and we then call
+     *    putListStatus(/a/b -> [/a/b/file2, /a/b/file3], isAuthoritative=true),
+     *    then we will end up with final state of
+     *    [/a/b/file0, /a/b/file1, /a/b/file2, /a/b/file3], isAuthoritative =
+     *    true
+     */
+    S3AFileStatus prevStatus = null;
+    // Iterate from leaf to root
+    for (int i = 0; i < dirs.size(); i++) {
+      boolean isLeaf = (prevStatus == null);
+      Path f = dirs.get(i);
+      assertQualified(f);
+      S3AFileStatus status = new S3AFileStatus(isLeaf, f, owner);
+      Collection<PathMetadata> children;
+      if (isLeaf) {
+        children = DirListingMetadata.EMPTY_DIR;
+      } else {
+        children = new ArrayList<>(1);
+        children.add(new PathMetadata(prevStatus));
+      }
+      DirListingMetadata dirMeta = new DirListingMetadata(f, children, true);
+      try {
+        ms.put(dirMeta);
+        ms.put(new PathMetadata(status));
+      } catch (IOException ioe) {
+        LOG.error("MetadataStore#put() failure:", ioe);
+        return;
+      }
+      prevStatus = status;
+    }
+  }
+
+  /**
+   * Helper function that records the move of directory path, adding
+   * resulting metadata to the supplied lists.  Does not store in MetadataStore.
+   * @param ms  MetadataStore, used to make this a no-op, when it is
+   *            NullMetadataStore.
+   * @param srcPaths stores the source path here
+   * @param dstMetas stores destination metadata here
+   * @param srcPath  source path to store
+   * @param dstPath  destination path to store
+   * @param owner Hadoop user name
+   */
+  public static void addMoveDir(MetadataStore ms, Collection<Path> srcPaths,
+      Collection<PathMetadata> dstMetas, Path srcPath, Path dstPath,
+      String owner) {
+    if (isNullMetadataStore(ms)) {
+      return;
+    }
+    assertQualified(srcPath);
+    assertQualified(dstPath);
+    S3AFileStatus dstStatus = new S3AFileStatus(true, dstPath, owner);
+    addMoveStatus(srcPaths, dstMetas, srcPath, dstStatus);
+  }
+
+  /**
+   * Like {@link #addMoveDir(MetadataStore, Collection, Collection, Path,
+   * Path, String)} (), but for files.
+   * @param ms  MetadataStore, used to make this a no-op, when it is
+   *            NullMetadataStore.
+   * @param srcPaths stores the source path here
+   * @param dstMetas stores destination metadata here
+   * @param srcPath  source path to store
+   * @param dstPath  destination path to store
+   * @param size length of file moved
+   * @param blockSize  blocksize to associate with destination file
+   * @param owner Hadoop user name
+   */
+  public static void addMoveFile(MetadataStore ms, Collection<Path> srcPaths,
+      Collection<PathMetadata> dstMetas, Path srcPath, Path dstPath,
+      long size, long blockSize, String owner) {
+    if (isNullMetadataStore(ms)) {
+      return;
+    }
+    assertQualified(srcPath);
+    assertQualified(dstPath);
+    S3AFileStatus dstStatus = S3AUtils.createUploadFileStatus(dstPath, false,
+        size, blockSize, owner);
+    addMoveStatus(srcPaths, dstMetas, srcPath, dstStatus);
+  }
+
+  private static void addMoveStatus(Collection<Path> srcPaths,
+      Collection<PathMetadata> dstMetas, Path srcPath, S3AFileStatus dstStatus)
+  {
+    srcPaths.add(srcPath);
+    dstMetas.add(new PathMetadata(dstStatus));
+  }
+
+  public static void assertQualified(Path p) {
+    URI uri = p.toUri();
+    // Paths must include bucket in case MetadataStore is shared between
+    // multiple S3AFileSystem instances
+    Preconditions.checkNotNull(uri.getHost());
+
+    // I believe this should never fail, since there is a host?
+    Preconditions.checkNotNull(uri.getScheme());
+  }
+}