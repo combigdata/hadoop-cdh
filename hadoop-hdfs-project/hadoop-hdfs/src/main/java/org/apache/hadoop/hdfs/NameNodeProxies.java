8f0724a7a75680200dbb1d495a08a11bbbe4bcce
Wei-Chiu Chuang <weichiu@cloudera.com>
2017-02-22 11:38:46 -0800

    Revert "HDFS-8307. Spurious DNS Queries from hdfs shell. Contributed by  Andres Perez"
    This reverts commit 2d5f476f5e7556e6091ed69acfaba3411588b840.
    
    Change-Id: Ic95e80484c8d112f2aa4ca237c60cba005a50d93

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/NameNodeProxies.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/NameNodeProxies.java
@@ -192,18 +192,8 @@ public class NameNodeProxies {
         dtService = SecurityUtil.buildTokenService(
             NameNode.getAddress(nameNodeUri));
       }
-
-      InetSocketAddress nnAddress;
-
-      //We dont need to resolve the address if is it a Nameservice ID
-      if(HAUtil.isLogicalUri(conf, nameNodeUri)) {
-        nnAddress = InetSocketAddress.createUnresolved(
-            nameNodeUri.getHost(), NameNode.DEFAULT_PORT);
-      } else {
-        nnAddress = NameNode.getAddressCheckLogical(conf, nameNodeUri);
-      }
-
-      return new ProxyAndInfo<T>(proxy, dtService, nnAddress);
+      return new ProxyAndInfo<T>(proxy, dtService,
+          NameNode.getAddressCheckLogical(conf, nameNodeUri));
     }
   }
   
