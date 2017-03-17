26dd73141e91d51344e0a599af37c7fa8e046021
Yufei Gu <yufei.gu@cloudera.com>
2017-03-17 09:18:28 -0800

    Revert "YARN-6042. Dump scheduler and queue state information into FairScheduler DEBUG log. (Yufei Gu via rchiang)"
    This reverts commit 9a3b78cf80dff8e0adbadd52c3daa8b9a9c02404.
    
    Change-Id: Ie305c1902b1af6403239e4d0bb2a09823a1cd14f

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/FSAppAttempt.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/FSAppAttempt.java
@@ -662,22 +662,14 @@ public class FSAppAttempt extends SchedulerApplicationAttempt
       return capability;
     }
 
-    if (LOG.isDebugEnabled()) {
-      LOG.debug("Resource request: " + capability + " exceeds the available"
-          + " resources of the node.");
-    }
-
     // The desired container won't fit here, so reserve
     if (isReservable(capability) &&
         reserve(request, node, reservedContainer, type)) {
-      if (LOG.isDebugEnabled()) {
-        LOG.debug(getName() + "'s resource request is reserved.");
-      }
       return FairScheduler.CONTAINER_RESERVED;
     } else {
       if (LOG.isDebugEnabled()) {
-        LOG.debug("Couldn't create reservation for app:  " + getName()
-            + ", at priority " +  request.getPriority());
+        LOG.debug("Couldn't creating reservation for " +
+            getName() + ",at priority " +  request.getPriority());
       }
       return Resources.none();
     }
@@ -956,17 +948,10 @@ public class FSAppAttempt extends SchedulerApplicationAttempt
   @Override
   public Resource assignContainer(FSSchedulerNode node) {
     if (isOverAMShareLimit()) {
-      List<ResourceRequest> ask = appSchedulingInfo.getAllResourceRequests();
-      Resource amResourceRequest = Resources.none();
-      if (!ask.isEmpty()) {
-        amResourceRequest = ask.get(0).getCapability();
-      }
       if (LOG.isDebugEnabled()) {
-        LOG.debug("AM resource request: " + amResourceRequest
-            + " exceeds maximum AM resource allowed, "
-            + getQueue().dumpState());
+        LOG.debug("Skipping allocation because maxAMShare limit would " +
+            "be exceeded");
       }
-
       return Resources.none();
     }
     return assignContainer(node, false);
