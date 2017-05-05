212241ac3b21f318768cfebe5fc00354fa288339
Yongjun Zhang <yzhang@cloudera.com>
2017-05-05 11:50:59 -0700

    Revert "HADOOP-14104. Client should always ask namenode for kms provider path. Contributed by Rushabh S Shah."
    This reverts commit 39c01398826ed6605fc44f25398d8c7649aaa5a7.
    
    Change-Id: I77cc42617be62db6972191d18804e2836654e76f

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocolPB/PBHelper.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocolPB/PBHelper.java
@@ -1345,8 +1345,7 @@ public class PBHelper {
         fs.getFileBufferSize(),
         fs.getEncryptDataTransfer(),
         fs.getTrashInterval(),
-        PBHelper.convert(fs.getChecksumType()),
-        fs.hasKeyProviderUri() ? fs.getKeyProviderUri() : null);
+        PBHelper.convert(fs.getChecksumType()));
   }
   
   public static FsServerDefaultsProto convert(FsServerDefaults fs) {
@@ -1360,7 +1359,6 @@ public class PBHelper {
       .setEncryptDataTransfer(fs.getEncryptDataTransfer())
       .setTrashInterval(fs.getTrashInterval())
       .setChecksumType(PBHelper.convert(fs.getChecksumType()))
-      .setKeyProviderUri(fs.getKeyProviderUri())
       .build();
   }
   
