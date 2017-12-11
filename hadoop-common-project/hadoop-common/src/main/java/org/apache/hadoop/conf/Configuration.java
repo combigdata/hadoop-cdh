2990314eeabb5a0cb24f47e3c08a3f543cc8f87e
Xiao Chen <xiao@cloudera.com>
2017-12-11 11:13:56 -0800

    CLOUDERA-BUILD. CDH-62093. Revert "Addendum patch for Configuration fix. (Jason Lowe via asuresh)"
    This reverts commit b07e68b02a34d272114dda4194992a847928aef8.
    
    Change-Id: I010cf3abf3830efeeb92d5884b1c1fa72d4fba3f

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/conf/Configuration.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/conf/Configuration.java
@@ -2936,8 +2936,7 @@ public class Configuration implements Iterable<Map.Entry<String,String>>,
             // xi:include are treated as inline and retain current source
             URL include = getResource(confInclude);
             if (include != null) {
-              Resource classpathResource = new Resource(include, name,
-                  wrapper.isParserRestricted());
+              Resource classpathResource = new Resource(include, name);
               loadResource(properties, classpathResource, quiet);
             } else {
               URL url;
@@ -2958,8 +2957,7 @@ public class Configuration implements Iterable<Map.Entry<String,String>>,
                 }
                 url = href.toURI().toURL();
               }
-              Resource uriResource = new Resource(url, name,
-                  wrapper.isParserRestricted());
+              Resource uriResource = new Resource(url, name);
               loadResource(properties, uriResource, quiet);
             }
             break;
