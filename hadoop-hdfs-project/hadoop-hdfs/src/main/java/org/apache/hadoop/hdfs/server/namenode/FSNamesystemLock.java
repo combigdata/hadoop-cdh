apache/hadoop

HDFS-13112. Token expiration edits may cause log corruption or deadlock. Contributed by Daryn Sharp.
(cherry picked from commit 47473952e56b0380147d42f4110ad03c2276c961)

hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystemLock.java
@@ -145,6 +145,13 @@ class FSNamesystemLock {
     }
   }
 
+  public void readLockInterruptibly() throws InterruptedException {
+    coarseLock.readLock().lockInterruptibly();
+    if (coarseLock.getReadHoldCount() == 1) {
+      readLockHeldTimeStampNanos.set(timer.monotonicNowNanos());
+    }
+  }
+
   public void readUnlock() {
     readUnlock(OP_NAME_OTHER);
   }

combigdata/hadoop-cdh

(cherry picked from commit 7f6ab3f9d06f829407e159614ec1837b6033a259)
 Conflicts:
	hadoop-hdfs-project/hadoop-hdfs/CHANGES.txt
Change-Id: I3ff9fbef354004d8e4473692a46548695c6312d5

hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystemLock.java
@@ -112,6 +112,13 @@ class FSNamesystemLock {
     }
   }
 
+  public void readLockInterruptibly() throws InterruptedException {
+    coarseLock.readLock().lockInterruptibly();
+    if (coarseLock.getReadHoldCount() == 1) {
+      readLockHeldTimeStamp.set(timer.monotonicNow());
+    }
+  }
+
   public void readUnlock() {
     final boolean needReport = coarseLock.getReadHoldCount() == 1;
     final long readLockInterval =