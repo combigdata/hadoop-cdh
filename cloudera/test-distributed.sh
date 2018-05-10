851fd4723dcb0f95053457a832888d3aa9cdbf8d
Xiao Chen <xiao@cloudera.com>
2018-05-10 09:41:25 -0700

    CLOUDERA-BUILD. CDH-64381. Enable dist-test for hadoop c6 pre-commit.
    Change-Id: Ifad439bf95305c333aca0deb18c585d93fd86cc8

--- cloudera/test-distributed.sh
+++ cloudera/test-distributed.sh
@@ -1,65 +1,14 @@
 #!/bin/bash
-
-# TODO: temporarily disable until grind with JDK8 works
-exit 0
-
 set -xe
 
 # activate mvn-gbn wrapper
-mv "$(which mvn-gbn-wrapper)" "$(dirname "$(which mvn-gbn-wrapper)")/mvn"
+# mv "$(which mvn-gbn-wrapper)" "$(dirname "$(which mvn-gbn-wrapper)")/mvn"
 
-DIR="$( cd $( dirname ${BASH_SOURCE[0]} )  && pwd )"
-cd $DIR
+# DIR="$( cd $( dirname ${BASH_SOURCE[0]} )  && pwd )"
+# cd $DIR
 
-# Build the project
-$DIR/build.sh
 
-# Install supertest locally
-SCRIPTS="supertest"
-
-if [[ -d $SCRIPTS ]]; then
-    echo "Cleaning up remnants from a previous run"
-    rm -rf $SCRIPTS
-fi
-
-git clone --depth 1 git://github.mtv.cloudera.com/CDH/$SCRIPTS.git $SCRIPTS || true
-
-# Fetch the right branch
-cd "$DIR/$SCRIPTS"
-git fetch --depth 1 origin
-git checkout -f origin/master
-git ls-tree -r HEAD
-./init.sh
-git submodule status
-./make-config.sh
-# Activate the supertest virtualenv
-source ./supertest-env/bin/activate
-export PATH=`pwd`/grind/bin/:$PATH
-which grind
-
-# Fetch dist test credentials and add them to the environment
-wget http://staging.jenkins.cloudera.com/gerrit-artifacts/misc/hadoop/dist_test_cred.sh
-source dist_test_cred.sh
-
-# Go to project root
-cd "$DIR/.."
-
-# Populate the per-project grind cfg file
-cat > .grind_project.cfg << EOF
-[grind]
-empty_dirs = ["test/data", "test-dir", "log"]
-file_globs = []
-file_patterns = ["*.so"]
-artifact_archive_globs = ["**/surefire-reports/TEST-*.xml"]
-EOF
-
-# Invoke grind to run tests
-grind -c ${DIR}/supertest/grind.cfg config
-grind -c ${DIR}/supertest/grind.cfg pconfig
-grind -c ${DIR}/supertest/grind.cfg test --artifacts -r 3 -e TestRM -e TestWorkPreservingRMRestart -e TestRMRestart -e TestContainerAllocation -e TestMRJobClient -e TestCapacityScheduler -e TestDelegatingInputFormat -e TestMRCJCFileInputFormat -e TestJobHistoryEventHandler -e TestCombineFileInputFormat -e TestAMRMRPCResponseId -e TestSystemMetricsPublisher -e TestNodesListManager -e TestRMContainerImpl -e TestApplicationMasterLauncher -e TestRMWebApp -e TestContainerManagerSecurity -e TestResourceManager -e TestParameterParser -e TestNativeCodeLoader -e TestRMContainerAllocator -e TestMRIntermediateDataEncryption -e TestWebApp -e TestCryptoStreamsWithOpensslAesCtrCryptoCodec -e TestDNS
-# TestDNS fails only on supertest. CDH-37451
-
-# Cleanup the grind folder
-if [[ -d "$DIR/$SCRIPTS" ]]; then
-    rm -rf "$DIR/$SCRIPTS"
-fi
+# invoke docker with an image that can build hadoop
+# namely we need: cmake 3.1+, mvn 3.3+, protobuf2.5+, java 8.
+# toolchain is currently used to conveniently setup most of these.
+docker run -v "$(pwd):/hadoop" -w /hadoop docker-registry.infra.cloudera.com/cauldron/ubuntu1604:1505254507 bash -c "adduser --uid $(id -u) --gecos '' --disabled-password pre_post_commit; su pre_post_commit -c '/hadoop/cloudera/inside-docker.sh'"
