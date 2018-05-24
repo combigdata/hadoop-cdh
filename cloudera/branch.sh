48f80a88ceac28d95ce73bfdaa7c9df730dadb24
Hector Acosta <hector.acosta@cloudera.com>
2018-05-24 09:31:11 -0700

    CLOUDERA-BUILD: Update parent version on cloudera/branch.sh
    Signed-off-by: Hector Acosta <hector.acosta@cloudera.com>
    (cherry picked from commit 586c3cfd8a53cc10cf34f9b23d9ddd3bb6c65978)
    Signed-off-by: Hector Acosta <hector.acosta@cloudera.com>
    
    Conflicts:
    	cloudera/branch.sh
    
    Change-Id: I482fd69e8d9769dfa9b416b5d4a5abbc28db244f

---
+++ cloudera/branch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -exu
+mvn --batch-mode versions:set -DnewVersion=3.0.0-cdh$CDH_NEW_MAVEN_VERSION
+sed -i "s/$CDH_START_VERSION/$CDH_NEW_VERSION/" cloudera/lib.sh
+# Update the parent version. mvn versions:update-parent is useless since new_maven_version may not exist yet
+perl -0777 -i -pe 's/(<version>).*(<\/version>\s*\n*<\/parent>)/\1$ENV{CDH_NEW_MAVEN_VERSION}\2/' pom.xml
+
+# Assert something changed and print the diff.
+! git diff --exit-code
+# Stage changes
+git add -u
