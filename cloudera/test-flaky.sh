d3ea0f78fcbed24d6c229bca835c83878d8ebbbf
George Huang <j.huang@cloudera.com>
2018-06-13 16:57:31 -0700

    CLOUDERA-BUILD.CDH-68554.Grouping HDFS unit tests with flakies, integrations and stables     category and integrate with Quasar
    Change-Id: Ia8ff7e44966e63e1d7e55c8bb3473128e602e753

--- cloudera/test-flaky.sh
+++ cloudera/test-flaky.sh
@@ -4,4 +4,5 @@ set -xe
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $DIR/lib.sh
+export MAVEN_FLAGS="-P flakies"
 main $@
--- cloudera/test-integration.sh
+++ cloudera/test-integration.sh
@@ -7,5 +7,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $DIR/lib.sh
 
-export MAVEN_FLAGS="-P IntegrationsTest"
+export MAVEN_FLAGS="-P integrations"
 main $@
