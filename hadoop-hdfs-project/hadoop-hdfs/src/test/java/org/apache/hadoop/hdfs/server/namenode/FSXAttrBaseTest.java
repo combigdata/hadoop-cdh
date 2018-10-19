1ac23379529ece506d476c504048e9691067c36f
Wei-Chiu Chuang <weichiu@cloudera.com>
2018-10-19 11:50:25 -0700

    Revert "xattr api cleanup"
    This reverts commit d897b944a80c576b32aa02f684bf36ea3f41840f.
    
    Change-Id: I622cae7c6f9c199ecb042654720b0f4c64c19832

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/FSXAttrBaseTest.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/FSXAttrBaseTest.java
@@ -841,37 +841,28 @@ public class FSXAttrBaseTest {
     }
 
     /*
-     * Check that execute/scan access to the parent dir is not
-     * sufficient to get xattr names.
+     * Check that execute/scan access to the parent dir is sufficient to get
+     * xattr names.
      */
     fs.setPermission(path, new FsPermission((short) 0701));
     user.doAs(new PrivilegedExceptionAction<Object>() {
         @Override
         public Object run() throws Exception {
-        try {
           final FileSystem userFs = dfsCluster.getFileSystem();
           userFs.listXAttrs(childDir);
-          fail("expected AccessControlException");
-        } catch (AccessControlException ace) {
-          GenericTestUtils.assertExceptionContains("Permission denied", ace);
+          return null;
         }
-        return null;
-      }
       });
 
     /*
      * Test that xattrs in the "trusted" namespace are filtered correctly.
      */
-    // Allow the user to read child path.
-    fs.setPermission(childDir, new FsPermission((short) 0704));
     fs.setXAttr(childDir, "trusted.myxattr", "1234".getBytes());
     user.doAs(new PrivilegedExceptionAction<Object>() {
         @Override
         public Object run() throws Exception {
           final FileSystem userFs = dfsCluster.getFileSystem();
-          List<String> xattrs = userFs.listXAttrs(childDir);
-          assertTrue(xattrs.size() == 1);
-          assertEquals(name1, xattrs.get(0));
+          assertTrue(userFs.listXAttrs(childDir).size() == 1);
           return null;
         }
       });
@@ -1115,24 +1106,20 @@ public class FSXAttrBaseTest {
             }
 
             /*
-            * Test that user who have parent directory execute access
-            *  can also not see raw.* xattrs returned from listXAttr
-            */
-            try {
-              // non-raw path
-              userFs.listXAttrs(path);
-              fail("listXAttr should have thrown AccessControlException");
-            } catch (AccessControlException ace) {
-              // expected
-            }
-
+             * Test that only root can see raw.* xattrs returned from listXAttr
+             * and non-root can't do listXAttrs on /.reserved/raw.
+             */
+            // non-raw path
+            final List<String> xattrNames = userFs.listXAttrs(path);
+            assertTrue(xattrNames.size() == 0);
             try {
               // raw path
               userFs.listXAttrs(rawPath);
-              fail("listXAttr should have thrown AccessControlException");
-            } catch (AccessControlException ace) {
-              // expected
+              fail("listXAttrs on raw path should have thrown");
+            } catch (AccessControlException e) {
+              // ignore
             }
+
             return null;
           }
         });
