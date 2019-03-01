5821209a0c70138f9d64798bf19468248e633d76
Bernadett Balogh <bbalogh@cloudera.com>
2019-03-01 15:53:30 +0100

    CLOUDERA-BUILD RELENG-4647 inside-docker.sh added to branch script
    This file contains a version specific url, which has to be updated
    during versioning. Previously this was done manually.
    
    Change-Id: I489f8570a6c30dfaac8cc38b5d9e68e459dce9ee

--- cloudera/branch.sh
+++ cloudera/branch.sh
@@ -2,7 +2,7 @@
 
 set -exu
 mvn --batch-mode versions:set -DnewVersion=3.0.0-cdh$CDH_NEW_MAVEN_VERSION
-sed -i "s/$CDH_START_VERSION/$CDH_NEW_VERSION/" cloudera/lib.sh
+sed -i "s/$CDH_START_VERSION/$CDH_NEW_VERSION/" cloudera/lib.sh cloudera/inside-docker.sh
 # Update the parent version. mvn versions:update-parent is useless since new_maven_version may not exist yet
 perl -0777 -i -pe 's/(<version>).*(<\/version>\s*\n*<\/parent>)/\1$ENV{CDH_NEW_MAVEN_VERSION}\2/' pom.xml
 
