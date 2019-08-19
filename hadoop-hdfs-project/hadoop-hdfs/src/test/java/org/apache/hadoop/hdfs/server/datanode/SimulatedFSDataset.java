387171d66513ba90ff50f0cb7aafc93e72ac4906
Wei-Chiu Chuang <weichiu@apache.org>
2019-08-19 13:08:55 -0700

    HDFS-13709. Report bad block to NN when transfer block encounter EIO exception. Contributed by Chen Zhang.
    Ref: CDH-81454
    
    (cherry picked from commit 360a96f342f3c8cb8246f011abb9bcb0b6ef3eaa)
    
     Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/DataNode.java
    
    (cherry picked from commit cf667aee96873634d5ae7270dbef86c365b1a4ec)
    Change-Id: I8a7e1553504c71f1a865b0bd60675b529aba9ac8

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/datanode/SimulatedFSDataset.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/datanode/SimulatedFSDataset.java
@@ -576,7 +576,7 @@ public class SimulatedFSDataset implements FsDatasetSpi<FsVolumeSpi> {
 
     @Override
     public StorageType getStorageType() {
-      return null;
+      return StorageType.DISK;
     }
 
     @Override
@@ -1160,7 +1160,7 @@ public class SimulatedFSDataset implements FsDatasetSpi<FsVolumeSpi> {
     return new ReplicaHandler(binfo, null);
   }
 
-  protected synchronized InputStream getBlockInputStream(ExtendedBlock b)
+  public synchronized InputStream getBlockInputStream(ExtendedBlock b)
       throws IOException {
     BInfo binfo = getBlockMap(b).get(b.getLocalBlock());
     if (binfo == null) {
